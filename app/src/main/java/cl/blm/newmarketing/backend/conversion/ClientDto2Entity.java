dbdd103b5125890707c641493fe7ef1f52e0fc99
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-08-21 18:01:39 -0400

    fixes client converters

--- app/src/main/java/cl/blm/newmarketing/backend/conversion/ClientDto2Entity.java
+++ app/src/main/java/cl/blm/newmarketing/backend/conversion/ClientDto2Entity.java
@@ -1,7 +1,5 @@
 package cl.blm.newmarketing.backend.conversion;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.convert.ConversionService;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
@@ -16,15 +14,13 @@ import cl.blm.newmarketing.backend.rest.dtos.ClientDto;
 @Component
 public class ClientDto2Entity
     implements Converter<ClientDto, Client> {
-  @Autowired
-  private ConversionService conversion;
 
   @Override
   public Client convert(ClientDto source) {
     Client target = new Client();
     target.setId(source.getClientId());
     if (source.getPerson() != null) {
-      Person person = conversion.convert(source.getPerson(), Person.class);
+      Person person = new PersonDto2Entity().convert(source.getPerson());
       target.setPerson(person);
     }
     return target;
--- app/src/main/java/cl/blm/newmarketing/backend/conversion/ClientPojo2Dto.java
+++ app/src/main/java/cl/blm/newmarketing/backend/conversion/ClientPojo2Dto.java
@@ -3,7 +3,7 @@ package cl.blm.newmarketing.backend.conversion;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
-import cl.blm.newmarketing.backend.model.entities.Client;
+import cl.blm.newmarketing.backend.pojos.ClientPojo;
 import cl.blm.newmarketing.backend.rest.dtos.ClientDto;
 import cl.blm.newmarketing.backend.rest.dtos.PersonDto;
 
@@ -12,14 +12,14 @@ import cl.blm.newmarketing.backend.rest.dtos.PersonDto;
  * @author Benjamin La Madrid <bg.lamadrid at gmail.com>
  */
 @Component
-public class ClientEntity2Dto
-    implements Converter<Client, ClientDto> {
+public class ClientPojo2Dto
+    implements Converter<ClientPojo, ClientDto> {
 
   @Override
-  public ClientDto convert(Client source) {
+  public ClientDto convert(ClientPojo source) {
     ClientDto target = new ClientDto();
-    target.setClientId(source.getId());
-    PersonDto person = new PersonEntity2Dto().convert(source.getPerson());
+    target.setClientId(source.id);
+    PersonDto person = new PersonPojo2Dto().convert(source.person);
     target.setPerson(person);
     return target;
   }
