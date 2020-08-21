22a4a146b8a5fbde103fb600ee93e39f2f76dc6a
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-08-21 13:34:25 -0400

    remove placeholder licenses

--- app/src/main/java/cl/blm/newmarketing/model/entities/ProductFamily.java
+++ app/src/main/java/cl/blm/newmarketing/model/entities/ProductFamily.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cl.blm.newmarketing.model.entities;
 
 import java.io.Serializable;
