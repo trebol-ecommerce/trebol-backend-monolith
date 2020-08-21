02b18c99496f64a6e1ebf3a009d99c08af6e52ad
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-08-21 13:56:10 -0400

    format dtos

--- app/src/main/java/cl/blm/newmarketing/rest/dtos/ClientDto.java
+++ app/src/main/java/cl/blm/newmarketing/rest/dtos/ClientDto.java
@@ -5,26 +5,26 @@ package cl.blm.newmarketing.rest.dtos;
  * @author Benjamin La Madrid <bg.lamadrid at gmail.com>
  */
 public class ClientDto {
-    private Integer clientId;
-    private PersonDto person;
+  private Integer clientId;
+  private PersonDto person;
 
-    public ClientDto() {
-      super();
-    }
+  public ClientDto() {
+    super();
+  }
 
-	public Integer getClientId() {
-		return clientId;
-	}
+  public Integer getClientId() {
+    return clientId;
+  }
 
-	public void setClientId(Integer clientId) {
-		this.clientId = clientId;
-	}
+  public void setClientId(Integer clientId) {
+    this.clientId = clientId;
+  }
 
-	public PersonDto getPerson() {
-		return person;
-	}
+  public PersonDto getPerson() {
+    return person;
+  }
 
-	public void setPerson(PersonDto person) {
-		this.person = person;
-	}
+  public void setPerson(PersonDto person) {
+    this.person = person;
+  }
 }
