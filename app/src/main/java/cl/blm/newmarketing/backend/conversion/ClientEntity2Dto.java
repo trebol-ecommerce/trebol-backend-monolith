afc2a5fe389cb48f847b7e835ecc9af64c74e761
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-08-21 16:24:01 -0400

    entity-recursive client conversion. autowired service works

--- app/src/main/java/cl/blm/newmarketing/backend/conversion/ClientEntity2Dto.java
+++ app/src/main/java/cl/blm/newmarketing/backend/conversion/ClientEntity2Dto.java
@@ -1,10 +1,13 @@
 package cl.blm.newmarketing.backend.conversion;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.ConversionService;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
 import cl.blm.newmarketing.backend.model.entities.Client;
 import cl.blm.newmarketing.backend.rest.dtos.ClientDto;
+import cl.blm.newmarketing.backend.rest.dtos.PersonDto;
 
 /**
  *
@@ -13,11 +16,15 @@ import cl.blm.newmarketing.backend.rest.dtos.ClientDto;
 @Component
 public class ClientEntity2Dto
     implements Converter<Client, ClientDto> {
+  @Autowired
+  private ConversionService conversion;
+
   @Override
   public ClientDto convert(Client source) {
     ClientDto target = new ClientDto();
     target.setClientId(source.getId());
-    // TODO can bind person too?
+    PersonDto person = conversion.convert(source.getPerson(), PersonDto.class);
+    target.setPerson(person);
     return target;
   }
 }
