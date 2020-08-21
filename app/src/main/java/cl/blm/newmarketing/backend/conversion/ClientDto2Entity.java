bffbe2cb16ae7dbfa331ad0323955f6577e04a99
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-08-21 16:27:21 -0400

    client-recursive dto-to-entity conversion

--- app/src/main/java/cl/blm/newmarketing/backend/conversion/ClientDto2Entity.java
+++ app/src/main/java/cl/blm/newmarketing/backend/conversion/ClientDto2Entity.java
@@ -1,9 +1,12 @@
 package cl.blm.newmarketing.backend.conversion;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.ConversionService;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
 import cl.blm.newmarketing.backend.model.entities.Client;
+import cl.blm.newmarketing.backend.model.entities.Person;
 import cl.blm.newmarketing.backend.rest.dtos.ClientDto;
 
 /**
@@ -13,11 +16,17 @@ import cl.blm.newmarketing.backend.rest.dtos.ClientDto;
 @Component
 public class ClientDto2Entity
     implements Converter<ClientDto, Client> {
+  @Autowired
+  private ConversionService conversion;
+
   @Override
   public Client convert(ClientDto source) {
     Client target = new Client();
     target.setId(source.getClientId());
-    // TODO can bind person too?
+    if (source.getPerson() != null) {
+      Person person = conversion.convert(source.getPerson(), Person.class);
+      target.setPerson(person);
+    }
     return target;
   }
 }
