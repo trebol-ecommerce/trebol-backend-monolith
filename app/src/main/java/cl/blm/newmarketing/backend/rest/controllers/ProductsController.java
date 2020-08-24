fc4ee457892b20b27704ba5cfa24b39c36f6d669
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-08-24 12:08:33 -0400

    refactor products controller to conform abstraction

--- app/src/main/java/cl/blm/newmarketing/backend/rest/controllers/ProductsController.java
+++ app/src/main/java/cl/blm/newmarketing/backend/rest/controllers/ProductsController.java
@@ -8,14 +8,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.ConversionService;
-import org.springframework.core.convert.TypeDescriptor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.querydsl.core.types.Predicate;
 
 import cl.blm.newmarketing.backend.BackendAppGlobals;
 import cl.blm.newmarketing.backend.pojos.ProductPojo;
@@ -29,15 +26,17 @@ import cl.blm.newmarketing.backend.rest.services.CrudService;
  */
 @RestController
 @RequestMapping("/api")
-public class ProductsController {
+public class ProductsController
+    extends EntityCrudController<ProductDto, Integer> {
   private final static Logger LOG = LoggerFactory.getLogger(ProductsController.class);
 
   @Autowired
   private ConversionService conversion;
+
   @Autowired
-  private CrudService<ProductDto, Integer> productService;
-  @Autowired
-  private BackendAppGlobals globals;
+  public ProductsController(BackendAppGlobals globals, CrudService<ProductDto, Integer> crudService) {
+    super(globals, crudService);
+  }
 
   @GetMapping("/products")
   public Collection<ProductPojo> read(@RequestParam Map<String, String> allRequestParams) {
@@ -50,39 +49,14 @@ public class ProductsController {
     return this.read(requestPageSize, null, allRequestParams);
   }
 
-  /**
-   * Retrieve a page of clients.
-   *
-   * @param requestPageSize
-   * @param requestPageIndex
-   * @param allRequestParams
-   *
-   * @see RequestParam
-   * @see Predicate
-   * @return
-   */
   @GetMapping("/products/{requestPageSize}/{requestPageIndex}")
   public Collection<ProductPojo> read(@PathVariable Integer requestPageSize, @PathVariable Integer requestPageIndex,
       @RequestParam Map<String, String> allRequestParams) {
     LOG.info("read");
-    Integer pageSize = globals.ITEMS_PER_PAGE;
-    Integer pageIndex = 0;
-    Predicate filters = null;
+    Collection<ProductDto> products = this.readFromService(requestPageSize, requestPageIndex, allRequestParams);
 
-    if (requestPageSize != null && requestPageSize > 0) {
-      pageSize = requestPageSize;
-    }
-    if (requestPageIndex != null && requestPageIndex > 0) {
-      pageIndex = requestPageIndex - 1;
-    }
-    if (allRequestParams != null && !allRequestParams.isEmpty()) {
-      filters = productService.queryParamsMapToPredicate(allRequestParams);
-    }
-
-    Collection<ProductDto> products = productService.read(pageSize, pageIndex, filters);
-    Collection<ProductPojo> productPojos = (List<ProductPojo>) conversion.convert(products,
-        TypeDescriptor.collection(Collection.class, TypeDescriptor.valueOf(ProductDto.class)),
-        TypeDescriptor.collection(List.class, TypeDescriptor.valueOf(ProductPojo.class)));
+    @SuppressWarnings("unchecked")
+    Collection<ProductPojo> productPojos = conversion.convert(products, List.class);
     return productPojos;
   }
 }
