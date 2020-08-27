037483fb55e5404eb393ddd93c93f71177856c30
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-08-27 12:54:51 -0400

    add dto converters for sell & related entities

---
+++ app/src/main/java/cl/blm/newmarketing/backend/converters/SellEntity2Dto.java
@@ -0,0 +1,45 @@
+package cl.blm.newmarketing.backend.converters;
+
+import java.util.Collection;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+import cl.blm.newmarketing.backend.dtos.ClientDto;
+import cl.blm.newmarketing.backend.dtos.SellDetailDto;
+import cl.blm.newmarketing.backend.dtos.SellDto;
+import cl.blm.newmarketing.backend.dtos.SellTypeDto;
+import cl.blm.newmarketing.backend.dtos.SellerDto;
+import cl.blm.newmarketing.backend.model.entities.Sell;
+
+/**
+ *
+ * @author Benjamin La Madrid <bg.lamadrid at gmail.com>
+ */
+@Component
+public class SellEntity2Dto
+    implements Converter<Sell, SellDto> {
+  @Override
+  public SellDto convert(Sell source) {
+    SellDto target = new SellDto();
+    target.setSellId(source.getId());
+    target.setSellDate(source.getDate());
+    target.setSellSubtotal(source.getSubtotal());
+
+    SellTypeDto sellType = (new SellTypeEntity2Dto()).convert(source.getSellType());
+    target.setSellType(sellType);
+
+    ClientDto client = (new ClientEntity2Dto()).convert(source.getClient());
+    target.setClient(client);
+
+    Collection<SellDetailDto> sellDetails = (new SellDetailEntity2Dto()).convertCollection(source.getSellDetails());
+    target.setSellDetails(sellDetails);
+
+    if (source.getSeller() != null) {
+      SellerDto seller = (new SellerEntity2Dto()).convert(source.getSeller());
+      target.setSeller(seller);
+    }
+
+    return target;
+  }
+}
