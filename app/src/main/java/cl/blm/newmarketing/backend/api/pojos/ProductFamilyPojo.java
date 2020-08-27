410f8058077416b85804f420d985edc933b45270
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-08-27 19:34:21 -0400

    add pojos for product type and family

---
+++ app/src/main/java/cl/blm/newmarketing/backend/api/pojos/ProductFamilyPojo.java
@@ -0,0 +1,6 @@
+package cl.blm.newmarketing.backend.api.pojos;
+
+public class ProductFamilyPojo {
+  public Integer id;
+  public String name;
+}
---
+++ app/src/main/java/cl/blm/newmarketing/backend/api/pojos/ProductTypePojo.java
@@ -0,0 +1,7 @@
+package cl.blm.newmarketing.backend.api.pojos;
+
+public class ProductTypePojo {
+  public Integer id;
+  public String name;
+  public ProductFamilyPojo productFamily;
+}
