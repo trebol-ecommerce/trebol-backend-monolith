0eb09b9be0806afec6add717f5fd1defe24da2fd
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-08-29 10:30:36 -0400

    fix client data fetch missing 'person' data

--- app/src/main/java/cl/blm/newmarketing/backend/model/repositories/ClientsRepository.java
+++ app/src/main/java/cl/blm/newmarketing/backend/model/repositories/ClientsRepository.java
@@ -1,6 +1,11 @@
 package cl.blm.newmarketing.backend.model.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import com.querydsl.core.types.Predicate;
 
 import cl.blm.newmarketing.backend.model.GenericRepository;
 import cl.blm.newmarketing.backend.model.entities.Client;
@@ -13,4 +18,9 @@ import cl.blm.newmarketing.backend.model.entities.Client;
 public interface ClientsRepository
     extends GenericRepository<Client, Integer> {
 
+  @Query(value = "SELECT c FROM Client c JOIN FETCH c.person", countQuery = "SELECT c FROM Client c")
+  Page<Client> deepReadAll(Pageable pageable);
+
+  @Query(value = "SELECT c FROM Client c JOIN FETCH c.person", countQuery = "SELECT c FROM Client c")
+  Page<Client> deepReadAll(Predicate filters, Pageable pageable);
 }
