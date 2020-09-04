508032c0d41c9e36d9237a70ae888c6420ca9b60
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-09-03 20:59:56 -0400

    implement all crud controller methods in abstract class

--- app/src/main/java/cl/blm/newmarketing/backend/api/GenericEntityQueryController.java
+++ app/src/main/java/cl/blm/newmarketing/backend/api/GenericEntityQueryController.java
@@ -3,6 +3,8 @@ package cl.blm.newmarketing.backend.api;
 import java.util.Collection;
 import java.util.Map;
 
+import org.slf4j.Logger;
+
 import com.querydsl.core.types.Predicate;
 
 import cl.blm.newmarketing.backend.CustomProperties;
@@ -14,24 +16,49 @@ import cl.blm.newmarketing.backend.services.data.GenericDataService;
  *
  * @author Benjamin La Madrid <bg.lamadrid at gmail.com>
  */
-public abstract class GenericEntityQueryController<P, E extends GenericEntity<I>, I> {
-
+public abstract class GenericEntityQueryController<P, E extends GenericEntity<I>, I>
+    implements GenericDataController<P, I> {
+  protected static Logger LOG;
   protected CustomProperties globals;
   protected GenericDataService<P, E, I> dataService;
 
-  public GenericEntityQueryController(CustomProperties globals, GenericDataService<P, E, I> dataService) {
+  public GenericEntityQueryController(Logger logger, CustomProperties globals,
+      GenericDataService<P, E, I> dataService) {
+    LOG = logger;
     this.globals = globals;
     this.dataService = dataService;
   }
 
   /**
-   * Retrieve a page of entities from the corresponding service. For filtering,
-   * transforms a Map into a Predicate.
+   * Pass a new item to the service.
+   * 
+   * @return The resulting item's ID
+   */
+  public I create(P input) {
+    LOG.info("create");
+    I resultId = dataService.create(input);
+    return resultId;
+  }
+
+  /**
+   * Fetch one item by a provided id.
+   *
+   * @param id The identifier of the item
+   * @return The item
+   */
+  public P readOne(I id) {
+    LOG.info("read");
+    P found = dataService.find(id);
+    return found;
+  }
+
+  /**
+   * Retrieve a page of items of with a fixed size and initial index. An optional
+   * Map (like query string parameters) can be provided for filtering criteria.
    *
    * @param requestPageSize
    * @param requestPageIndex
    * @param allRequestParams
-   *
    * @see Predicate
    * @return
    */
@@ -55,5 +82,30 @@ public abstract class GenericEntityQueryController<P, E extends GenericEntity<I>
     }
 
     return dataService.read(pageSize, pageIndex, filters);
+  }
+
+  /**
+   * Update the item found with an id, using data provided as input.
+   * 
+   * @param input
+   * @param id
+   * @return
+   */
+  public I update(P input, I id) {
+    LOG.info("update");
+    I resultId = dataService.update(input, id);
+    return resultId;
+  }
+
+  /**
+   * Delete an item by its id.
+   * 
+   * @param id
+   * @return
+   */
+  public boolean delete(I id) {
+    LOG.info("delete");
+    boolean result = dataService.delete(id);
+    return result;
   }
 }
