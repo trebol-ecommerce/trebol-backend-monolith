dc143ac7fbb20e362d685da3e88fe920c111d827
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-09-06 19:09:16 -0300

    redundant annotation

--- app/src/main/java/cl/blm/newmarketing/backend/BackendApp.java
+++ app/src/main/java/cl/blm/newmarketing/backend/BackendApp.java
@@ -2,7 +2,6 @@ package cl.blm.newmarketing.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 
@@ -11,7 +10,6 @@ import org.springframework.context.annotation.PropertySources;
  * @author Benjamin La Madrid <bg.lamadrid at gmail.com>
  */
 @SpringBootApplication
-@ComponentScan
 @PropertySources({ @PropertySource("classpath:logging.properties") })
 public class BackendApp {
   /**
