8dfabdd7f4c0f42c43d20c04d1ba38a1d7f7c5c2
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-09-06 19:11:15 -0300

    add initial test class

---
+++ app/src/test/java/cl/blm/newmarketing/backend/BackendTestApp.java
@@ -0,0 +1,12 @@
+package cl.blm.newmarketing.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTestApp {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BackendTestApp.class, args);
+  }
+}
