14ce11b1afffe7e3166d90f691c3793670bdb252
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-09-10 15:46:53 -0300

    separate configuration into classes in a new package

--- app/src/main/java/cl/blm/newmarketing/backend/config/ObjectMapperConfig.java
+++ app/src/main/java/cl/blm/newmarketing/backend/config/ObjectMapperConfig.java
@@ -1,11 +1,7 @@
-package cl.blm.newmarketing.backend.api;
+package cl.blm.newmarketing.backend.config;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
@@ -17,9 +13,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  * @author Benjamin La Madrid <bg.lamadrid at gmail.com>
  */
 @Configuration
-public abstract class ApiConfig
-    implements WebMvcConfigurer {
-  private Logger LOG = LoggerFactory.getLogger(ApiConfig.class);
+public abstract class ObjectMapperConfig {
 
   // JSON parser / serializer
   @Bean
@@ -28,14 +22,6 @@ public abstract class ApiConfig
     mapper.setSerializationInclusion(JsonInclude.Include.NON_DEFAULT);
     mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     return mapper;
-  }
-
-  // CORS configuration
-  @Override
-  public void addCorsMappings(CorsRegistry registry) {
-    LOG.debug("addCorsMappings");
-    registry.addMapping("/api/*").allowedOrigins("http://localhost:4200") // angular app
-        .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE").exposedHeaders("Authorization");
   }
 
 }
