90291ac9278a649f42b66618e5863e548c5b7578
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-09-11 12:46:26 -0300

    add QueryDSL and persistence.xml info on Permission/UserRolePermission

---
+++ app/src/main/generated-sources/cl/blm/newmarketing/backend/jpa/entities/QPermission.java
@@ -0,0 +1,41 @@
+package cl.blm.newmarketing.backend.jpa.entities;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QPermission is a Querydsl query type for Permission
+ */
+@Generated("com.querydsl.codegen.EntitySerializer")
+public class QPermission extends EntityPathBase<Permission> {
+
+    private static final long serialVersionUID = -35973513L;
+
+    public static final QPermission permission = new QPermission("permission");
+
+    public final StringPath code = createString("code");
+
+    public final StringPath description = createString("description");
+
+    public final NumberPath<Integer> id = createNumber("id", Integer.class);
+
+    public QPermission(String variable) {
+        super(Permission.class, forVariable(variable));
+    }
+
+    public QPermission(Path<? extends Permission> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QPermission(PathMetadata metadata) {
+        super(Permission.class, metadata);
+    }
+
+}
+
---
+++ app/src/main/generated-sources/cl/blm/newmarketing/backend/jpa/entities/QUserRolePermission.java
@@ -0,0 +1,54 @@
+package cl.blm.newmarketing.backend.jpa.entities;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QUserRolePermission is a Querydsl query type for UserRolePermission
+ */
+@Generated("com.querydsl.codegen.EntitySerializer")
+public class QUserRolePermission extends EntityPathBase<UserRolePermission> {
+
+    private static final long serialVersionUID = 1971939384L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QUserRolePermission userRolePermission = new QUserRolePermission("userRolePermission");
+
+    public final NumberPath<Integer> id = createNumber("id", Integer.class);
+
+    public final QPermission permission;
+
+    public final QUserRole userRole;
+
+    public QUserRolePermission(String variable) {
+        this(UserRolePermission.class, forVariable(variable), INITS);
+    }
+
+    public QUserRolePermission(Path<? extends UserRolePermission> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QUserRolePermission(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QUserRolePermission(PathMetadata metadata, PathInits inits) {
+        this(UserRolePermission.class, metadata, inits);
+    }
+
+    public QUserRolePermission(Class<? extends UserRolePermission> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.permission = inits.isInitialized("permission") ? new QPermission(forProperty("permission")) : null;
+        this.userRole = inits.isInitialized("userRole") ? new QUserRole(forProperty("userRole")) : null;
+    }
+
+}
+
