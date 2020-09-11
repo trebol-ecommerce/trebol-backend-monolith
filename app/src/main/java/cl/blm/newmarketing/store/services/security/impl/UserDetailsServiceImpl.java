efee217049e5442b84d549ed2ab4798f8e1c1466
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-09-11 15:52:01 -0300

    add user details service implementation - must finish

---
+++ app/src/main/java/cl/blm/newmarketing/store/services/security/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,59 @@
+package cl.blm.newmarketing.store.services.security.impl;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.querydsl.core.types.Predicate;
+
+import cl.blm.newmarketing.store.jpa.entities.Permission;
+import cl.blm.newmarketing.store.jpa.entities.QUserRolePermission;
+import cl.blm.newmarketing.store.jpa.entities.User;
+import cl.blm.newmarketing.store.jpa.entities.UserRole;
+import cl.blm.newmarketing.store.jpa.entities.UserRolePermission;
+import cl.blm.newmarketing.store.jpa.repositories.UserRolePermissionsRepository;
+import cl.blm.newmarketing.store.jpa.repositories.UsersRepository;
+
+@Service
+public class UserDetailsServiceImpl
+    implements UserDetailsService {
+
+  @Autowired
+  private UsersRepository usersRepository;
+
+  @Autowired
+  private UserRolePermissionsRepository userRolePermissionsRepository;
+
+  private Collection<Permission> getAllUserRolePermissions(User source) {
+    UserRole sourceRole = source.getUserRole();
+    Predicate qPermission = QUserRolePermission.userRolePermission.userRole.eq(sourceRole);
+
+    List<Permission> targetList = new ArrayList<>();
+    for (UserRolePermission permissions : userRolePermissionsRepository.findAll(qPermission)) {
+
+    }
+
+    return targetList;
+  }
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    Optional<User> foundUser = usersRepository.findByName(username);
+    if (foundUser.isPresent()) {
+      // TODO finish implementation
+      User user = foundUser.get();
+//      UserDetailsPojo userDetails = new UserDetailsPojo();
+      return null;
+    } else {
+      throw new UsernameNotFoundException(username);
+    }
+  }
+
+}
