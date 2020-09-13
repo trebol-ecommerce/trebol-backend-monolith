48a6e0ed8a9b6d519c0eba2903f589b70c3bfdd5
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-09-13 00:50:55 -0300

    comment out security filters - temporary full api access

--- app/src/main/java/cl/blm/newmarketing/store/config/SecurityConfig.java
+++ app/src/main/java/cl/blm/newmarketing/store/config/SecurityConfig.java
@@ -1,14 +1,18 @@
 package cl.blm.newmarketing.store.config;
 
+import javax.crypto.SecretKey;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-
-import cl.blm.newmarketing.store.services.security.impl.UserDetailsServiceImpl;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -16,16 +20,49 @@ import cl.blm.newmarketing.store.services.security.impl.UserDetailsServiceImpl;
 public class SecurityConfig
     extends WebSecurityConfigurerAdapter {
 
+  private final PasswordEncoder passwordEncoder;
+  private final UserDetailsService userDetailsService;
+  private final SecretKey secretKey;
+  private final JwtProperties jwtConfig;
+
+  @Autowired
+  public SecurityConfig(
+      PasswordEncoder passwordEncoder,
+      UserDetailsService userDetailsService,
+      SecretKey secretKey,
+      JwtProperties jwtConfig) {
+    super();
+    this.passwordEncoder = passwordEncoder;
+    this.userDetailsService = userDetailsService;
+    this.secretKey = secretKey;
+    this.jwtConfig = jwtConfig;
+  }
+
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http
-        .antMatcher("/api/*").authorizeRequests().anyRequest().authenticated();
+        .authorizeRequests().anyRequest().permitAll();
+//        .sessionManagement()
+//        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//        .and()
+//        .addFilter(new JwtUsernamePasswordAuthenticationFilter(authenticationManager(), jwtConfig, secretKey))
+//        .authorizeRequests()
+//        .antMatchers("/login").anonymous()
+//        .antMatchers("/api/**").authenticated()
+//        .anyRequest().authenticated();
+  }
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.authenticationProvider(daoAuthenticationProvider());
   }
 
   @Bean
-  @Override
-  protected UserDetailsService userDetailsService() {
-    return new UserDetailsServiceImpl();
+  public DaoAuthenticationProvider daoAuthenticationProvider() {
+    DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+    provider.setPasswordEncoder(passwordEncoder);
+    provider.setUserDetailsService(userDetailsService);
+    return provider;
   }
 
 }
