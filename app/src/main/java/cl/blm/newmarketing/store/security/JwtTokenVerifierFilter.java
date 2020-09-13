8a7795ffdfeb3c5793c05ca374ae00ae22a033ca
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-09-13 00:42:51 -0300

    token verifier filter should use new classes

--- app/src/main/java/cl/blm/newmarketing/store/security/JwtTokenVerifierFilter.java
+++ app/src/main/java/cl/blm/newmarketing/store/security/JwtTokenVerifierFilter.java
@@ -8,7 +8,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import io.jsonwebtoken.JwtException;
@@ -16,24 +15,32 @@ import io.jsonwebtoken.Jwts;
 
 import com.google.common.base.Strings;
 
+import cl.blm.newmarketing.store.config.JwtProperties;
+
 public class JwtTokenVerifierFilter
     extends OncePerRequestFilter {
 
-  @Autowired
-  private SecretKey secretKey;
+  private final SecretKey secretKey;
+  private final JwtProperties jwtProperties;
+
+  public JwtTokenVerifierFilter(SecretKey secretKey, JwtProperties jwtProperties) {
+    super();
+    this.secretKey = secretKey;
+    this.jwtProperties = jwtProperties;
+  }
 
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
       throws ServletException, IOException {
 
-    String authorizationHeader = request.getHeader("Authorization");
+    String authorizationHeader = request.getHeader(jwtProperties.getAuthorizationHeader());
 
-    if (Strings.isNullOrEmpty(authorizationHeader) || !authorizationHeader.startsWith("Bearer ")) {
+    if (Strings.isNullOrEmpty(authorizationHeader) || !authorizationHeader.startsWith(jwtProperties.getTokenPrefix())) {
       filterChain.doFilter(request, response);
       return;
     }
 
-    String token = authorizationHeader.replace("Bearer ", "");
+    String token = authorizationHeader.replace(jwtProperties.getTokenPrefix(), "");
 
     try {
       Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token);
