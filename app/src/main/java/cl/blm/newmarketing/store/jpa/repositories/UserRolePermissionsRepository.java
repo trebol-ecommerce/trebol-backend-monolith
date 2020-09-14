4494496dcdfc99b4717bf8199ce6268331791b1b
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-09-14 17:03:27 -0300

    add query for fetching user role permissions

--- app/src/main/java/cl/blm/newmarketing/store/jpa/repositories/UserRolePermissionsRepository.java
+++ app/src/main/java/cl/blm/newmarketing/store/jpa/repositories/UserRolePermissionsRepository.java
@@ -1,5 +1,7 @@
 package cl.blm.newmarketing.store.jpa.repositories;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import cl.blm.newmarketing.store.jpa.GenericRepository;
@@ -13,4 +15,6 @@ import cl.blm.newmarketing.store.jpa.entities.UserRolePermission;
 public interface UserRolePermissionsRepository
     extends GenericRepository<UserRolePermission, Integer> {
 
+  @Query("SELECT urp FROM UserRolePermission urp JOIN FETCH urp.permission WHERE urp.userRole.id = :userRoleId")
+  public Iterable<UserRolePermission> deepFindPermissionsByUserRoleId(@Param("userRoleId") Integer userRoleId);
 }
