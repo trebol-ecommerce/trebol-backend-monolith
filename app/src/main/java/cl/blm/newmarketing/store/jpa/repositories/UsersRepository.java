97b6098e47975feb2521073bc0c95c1e80dec586
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-09-14 17:03:38 -0300

    add query for fetching user with role by name

--- app/src/main/java/cl/blm/newmarketing/store/jpa/repositories/UsersRepository.java
+++ app/src/main/java/cl/blm/newmarketing/store/jpa/repositories/UsersRepository.java
@@ -2,6 +2,8 @@ package cl.blm.newmarketing.store.jpa.repositories;
 
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import cl.blm.newmarketing.store.jpa.GenericRepository;
@@ -15,5 +17,6 @@ import cl.blm.newmarketing.store.jpa.entities.User;
 public interface UsersRepository
     extends GenericRepository<User, Integer> {
 
-  public Optional<User> findByName(String name);
+  @Query("SELECT u FROM User u JOIN FETCH u.userRole WHERE u.name = :name")
+  public Optional<User> findByNameWithRole(@Param("name") String name);
 }
