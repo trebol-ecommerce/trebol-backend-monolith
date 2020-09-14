02978ab52ea986a0ee06261feac5a5c102e0ce2c
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-09-14 17:01:57 -0300

    comment generic repository

--- app/src/main/java/cl/blm/newmarketing/store/jpa/GenericRepository.java
+++ app/src/main/java/cl/blm/newmarketing/store/jpa/GenericRepository.java
@@ -3,6 +3,14 @@ package cl.blm.newmarketing.store.jpa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
+/**
+ * Interface for JPA repositories with QueryDSL support.
+ * 
+ * @author Benjamin La Madrid <bg.lamadrid at gmail.com>
+ *
+ * @param <T> The entity class
+ * @param <I> The identifier class
+ */
 public interface GenericRepository<T extends GenericEntity<I>, I>
     extends JpaRepository<T, I>, QuerydslPredicateExecutor<T> {
 
