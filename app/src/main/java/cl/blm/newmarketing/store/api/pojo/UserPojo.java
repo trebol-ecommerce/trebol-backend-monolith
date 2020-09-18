0b79e5868bd9dfed859f547c4b4d2df2c0c4b5ba
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-09-18 01:18:00 -0300

    add service and pojo classes for users, unpolished

---
+++ app/src/main/java/cl/blm/newmarketing/store/api/pojo/UserPojo.java
@@ -0,0 +1,36 @@
+package cl.blm.newmarketing.store.api.pojo;
+
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+//TODO could a user pojo benefit from person pojo?
+/**
+ * 
+ * @author Benjamin La Madrid <bg.lamadrid at gmail.com>
+ */
+public class UserPojo {
+  @JsonInclude
+  private Integer id;
+  @JsonInclude(value = Include.NON_DEFAULT)
+  @NotNull
+  private String name;
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+}
