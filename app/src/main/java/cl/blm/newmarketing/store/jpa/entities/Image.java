e5c4de1c2c8897746f99a564221fafac9344bee5
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-09-25 20:26:08 -0300

    add new entity classes

---
+++ app/src/main/java/cl/blm/newmarketing/store/jpa/entities/Image.java
@@ -0,0 +1,92 @@
+package cl.blm.newmarketing.store.jpa.entities;
+
+import cl.blm.newmarketing.store.jpa.GenericEntity;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author Benjamin La Madrid <bg.lamadrid@gmail.com>
+ */
+@Entity
+@Table(name = "images")
+@NamedQueries({
+    @NamedQuery(name = "Images.findAll", query = "SELECT i FROM Images i")})
+public class Image
+    implements GenericEntity<Integer> {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "image_id")
+    private Integer id;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 500)
+    @Column(name = "image_url")
+    private String url;
+
+    public Image() {
+    }
+
+    public Image(Integer imageId) {
+        this.id = imageId;
+    }
+
+    public Image(Integer imageId, String imageUrl) {
+        this.id = imageId;
+        this.url = imageUrl;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Image)) {
+            return false;
+        }
+        Image other = (Image) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "cl.blm.newmarketing.store.jpa.entities.Images[ imageId=" + id + " ]";
+    }
+
+}
