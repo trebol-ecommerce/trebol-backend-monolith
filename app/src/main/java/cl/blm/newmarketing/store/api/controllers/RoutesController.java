b06c4755b8d87a1eb500dd1ff7433a30931c45e0
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-09-28 16:04:15 -0300

    add methods for querying api access by route

--- app/src/main/java/cl/blm/newmarketing/store/api/controllers/RoutesController.java
+++ app/src/main/java/cl/blm/newmarketing/store/api/controllers/RoutesController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,17 +33,109 @@ public class RoutesController {
     this.routeService = routeService;
   }
 
-  @GetMapping("/api")
-  public AuthorizedAccessPojo getApiRoutes(@RequestHeader HttpHeaders requestHeaders) {
-    String authorizationHeader = jwtClaimsParserService.extractAuthorizationHeader(requestHeaders);
+  private UserDetails getUserDetails(String authorizationHeader) throws UsernameNotFoundException {
+    Claims body = jwtClaimsParserService.parseToken(authorizationHeader);
+    String username = body.getSubject();
+    UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+    return userDetails;
+  }
 
+  private AuthorizedAccessPojo loadApiRouteAccess(String authorizationHeader, String apiRoute) throws UsernameNotFoundException {
+    UserDetails userDetails = getUserDetails(authorizationHeader);
+    Collection<String> permissions = routeService.getAuthorizedApiRouteAccess(userDetails, apiRoute);
+    AuthorizedAccessPojo target = new AuthorizedAccessPojo();
+    target.setPermissions(permissions);
+    return target;
+  }
+
+  @GetMapping("/api")
+  public AuthorizedAccessPojo getApiRoutesAccess(@RequestHeader HttpHeaders requestHeaders) {
+    String authorizationHeader = jwtClaimsParserService.extractAuthorizationHeader(requestHeaders);
     if (authorizationHeader != null) {
-      Claims body = jwtClaimsParserService.parseToken(authorizationHeader);
-      String username = body.getSubject();
-      UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+      UserDetails userDetails = getUserDetails(authorizationHeader);
       Collection<String> routes = routeService.getAuthorizedApiRoutes(userDetails);
       AuthorizedAccessPojo target = new AuthorizedAccessPojo();
       target.setRoutes(routes);
+      return target;
+    }
+    return null;
+  }
+
+  @GetMapping("/api/client")
+  public AuthorizedAccessPojo getClientApiAccess(@RequestHeader HttpHeaders requestHeaders) {
+    String authorizationHeader = jwtClaimsParserService.extractAuthorizationHeader(requestHeaders);
+    if (authorizationHeader != null) {
+      AuthorizedAccessPojo target = loadApiRouteAccess(authorizationHeader, "clients");
+      return target;
+    }
+    return null;
+  }
+
+  @GetMapping("/api/person")
+  public AuthorizedAccessPojo getPersonApiAccess(@RequestHeader HttpHeaders requestHeaders) {
+    String authorizationHeader = jwtClaimsParserService.extractAuthorizationHeader(requestHeaders);
+    if (authorizationHeader != null) {
+      AuthorizedAccessPojo target = loadApiRouteAccess(authorizationHeader, "people");
+      return target;
+    }
+    return null;
+  }
+
+  @GetMapping("/api/product")
+  public AuthorizedAccessPojo getProductApiAccess(@RequestHeader HttpHeaders requestHeaders) {
+    String authorizationHeader = jwtClaimsParserService.extractAuthorizationHeader(requestHeaders);
+    if (authorizationHeader != null) {
+      AuthorizedAccessPojo target = loadApiRouteAccess(authorizationHeader, "products");
+      return target;
+    }
+    return null;
+  }
+
+  @GetMapping("/api/product_type")
+  public AuthorizedAccessPojo getProductTypeApiAccess(@RequestHeader HttpHeaders requestHeaders) {
+    String authorizationHeader = jwtClaimsParserService.extractAuthorizationHeader(requestHeaders);
+    if (authorizationHeader != null) {
+      AuthorizedAccessPojo target = loadApiRouteAccess(authorizationHeader, "product_types");
+      return target;
+    }
+    return null;
+  }
+
+  @GetMapping("/api/product_family")
+  public AuthorizedAccessPojo getProductFamilyApiAccess(@RequestHeader HttpHeaders requestHeaders) {
+    String authorizationHeader = jwtClaimsParserService.extractAuthorizationHeader(requestHeaders);
+    if (authorizationHeader != null) {
+      AuthorizedAccessPojo target = loadApiRouteAccess(authorizationHeader, "product_families");
+      return target;
+    }
+    return null;
+  }
+
+  @GetMapping("/api/sell")
+  public AuthorizedAccessPojo getSellApiAccess(@RequestHeader HttpHeaders requestHeaders) {
+    String authorizationHeader = jwtClaimsParserService.extractAuthorizationHeader(requestHeaders);
+    if (authorizationHeader != null) {
+      AuthorizedAccessPojo target = loadApiRouteAccess(authorizationHeader, "sales");
+      return target;
+    }
+    return null;
+  }
+
+  @GetMapping("/api/sell_type")
+  public AuthorizedAccessPojo getSellTypeApiAccess(@RequestHeader HttpHeaders requestHeaders) {
+    String authorizationHeader = jwtClaimsParserService.extractAuthorizationHeader(requestHeaders);
+    if (authorizationHeader != null) {
+      AuthorizedAccessPojo target = loadApiRouteAccess(authorizationHeader, "sell_types");
+      return target;
+    }
+    return null;
+  }
+
+  @GetMapping("/api/seller")
+  public AuthorizedAccessPojo getSellerApiAccess(@RequestHeader HttpHeaders requestHeaders) {
+    String authorizationHeader = jwtClaimsParserService.extractAuthorizationHeader(requestHeaders);
+    if (authorizationHeader != null) {
+      AuthorizedAccessPojo target = loadApiRouteAccess(authorizationHeader, "sellers");
       return target;
     }
     return null;
