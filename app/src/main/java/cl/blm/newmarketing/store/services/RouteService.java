778b345acefbbee617ff0791257084a07cbcb8c2
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-09-27 22:22:49 -0300

    add service for querying available/authorized routes

---
+++ app/src/main/java/cl/blm/newmarketing/store/services/RouteService.java
@@ -0,0 +1,13 @@
+package cl.blm.newmarketing.store.services;
+
+import java.util.Collection;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+/**
+ *
+ * @author Benjamin La Madrid <bg.lamadrid@gmail.com>
+ */
+public interface RouteService {
+  public Collection<String> getAuthorizedApiRoutes(UserDetails userDetails);
+}
