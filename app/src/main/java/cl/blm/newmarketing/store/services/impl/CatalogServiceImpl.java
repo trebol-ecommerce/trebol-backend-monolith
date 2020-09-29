35fb7ca63606a49cade3537915f874d0066c6c71
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-09-28 22:38:45 -0300

    implement catalog service (using crud services on behalf)

--- app/src/main/java/cl/blm/newmarketing/store/services/impl/CatalogServiceImpl.java
+++ app/src/main/java/cl/blm/newmarketing/store/services/impl/CatalogServiceImpl.java
@@ -3,12 +3,19 @@ package cl.blm.newmarketing.store.services.impl;
 import java.util.Collection;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.querydsl.core.types.Predicate;
 
 import cl.blm.newmarketing.store.api.pojo.ProductFamilyPojo;
 import cl.blm.newmarketing.store.api.pojo.ProductPojo;
 import cl.blm.newmarketing.store.api.pojo.ProductTypePojo;
+import cl.blm.newmarketing.store.jpa.entities.Product;
+import cl.blm.newmarketing.store.jpa.entities.ProductFamily;
+import cl.blm.newmarketing.store.jpa.entities.ProductType;
 import cl.blm.newmarketing.store.services.CatalogService;
+import cl.blm.newmarketing.store.services.crud.GenericEntityCrudService;
 
 /**
  *
@@ -18,24 +25,38 @@ import cl.blm.newmarketing.store.services.CatalogService;
 public class CatalogServiceImpl
     implements CatalogService {
 
+  private final GenericEntityCrudService<ProductFamilyPojo, ProductFamily, Integer> productFamiliesService;
+  private final GenericEntityCrudService<ProductTypePojo, ProductType, Integer> productTypesService;
+  private final GenericEntityCrudService<ProductPojo, Product, Integer> productsService;
+
+  @Autowired
+  public CatalogServiceImpl(GenericEntityCrudService<ProductFamilyPojo, ProductFamily, Integer> productFamiliesService,
+      GenericEntityCrudService<ProductTypePojo, ProductType, Integer> productTypesService,
+      GenericEntityCrudService<ProductPojo, Product, Integer> productsService) {
+    this.productFamiliesService = productFamiliesService;
+    this.productTypesService = productTypesService;
+    this.productsService = productsService;
+  }
+
   @Override
-  public Collection<ProductPojo> readProducts(Integer requestPageSize, Integer requestPageIndex, Map<String, String> allRequestParams) {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  public Collection<ProductPojo> readProducts(Integer requestPageSize, Integer requestPageIndex, Map<String, String> queryParamsMap) {
+    Predicate filters = productsService.queryParamsMapToPredicate(queryParamsMap);
+    return productsService.read(requestPageSize, requestPageIndex, filters);
   }
 
   @Override
   public ProductPojo readProduct(Integer id) {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    return productsService.find(id);
   }
 
   @Override
   public Collection<ProductTypePojo> readProductTypes() {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    return productTypesService.read(10, 0, null);
   }
 
   @Override
   public Collection<ProductFamilyPojo> readProductFamilies() {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    return productFamiliesService.read(10, 0, null);
   }
 
 }
