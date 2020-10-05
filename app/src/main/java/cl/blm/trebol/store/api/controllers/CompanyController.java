823a755787c2968a2b81d7fa325b3e2b4ef11eed
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-10-05 17:15:36 -0300

    added controller class with unique method for fetching company data

---
+++ app/src/main/java/cl/blm/trebol/store/api/controllers/CompanyController.java
@@ -0,0 +1,29 @@
+package cl.blm.trebol.store.api.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import cl.blm.trebol.store.api.pojo.CompanyDetailsPojo;
+import cl.blm.trebol.store.services.CompanyService;
+
+/**
+ *
+ * @author Benjamin La Madrid <bg.lamadrid@gmail.com>
+ */
+@RestController
+public class CompanyController {
+
+  private final CompanyService companyService;
+
+  @Autowired
+  public CompanyController(CompanyService companyService) {
+    this.companyService = companyService;
+  }
+
+  @GetMapping("/company")
+  public CompanyDetailsPojo readCompanyDetails() {
+    CompanyDetailsPojo companyDetailsPojo = companyService.readDetails();
+    return companyDetailsPojo;
+  }
+}
