14504b629de1276d035bcadf75b8c3db0631318d
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-10-05 17:25:45 -0300

    initial company service implementation

---
+++ app/src/main/java/cl/blm/trebol/store/services/impl/CompanyServiceImpl.java
@@ -0,0 +1,50 @@
+package cl.blm.trebol.store.services.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import cl.blm.trebol.store.api.pojo.CompanyDetailsPojo;
+import cl.blm.trebol.store.jpa.entities.Param;
+import cl.blm.trebol.store.jpa.repositories.ParamsRepository;
+import cl.blm.trebol.store.services.CompanyService;
+
+/**
+ *
+ * @author Benjamin La Madrid <bg.lamadrid@gmail.com>
+ */
+public class CompanyServiceImpl
+    implements CompanyService {
+
+  private final ParamsRepository paramsRepository;
+
+  @Autowired
+  public CompanyServiceImpl(ParamsRepository paramsRepository) {
+    this.paramsRepository = paramsRepository;
+  }
+
+  @Override
+  public CompanyDetailsPojo readDetails() {
+    Iterable<Param> it = paramsRepository.findParamsByCategory("company");
+    CompanyDetailsPojo target = new CompanyDetailsPojo();
+    for (Param p : it) {
+      String v = p.getValue();
+      switch (p.getName()) {
+        case "name":
+          target.setName(v);
+          break;
+        case "description":
+          target.setDescription(v);
+          break;
+        case "bannerImageURL":
+          target.setBannerImageURL(v);
+          break;
+        case "logoImageURL":
+          target.setLogoImageURL(v);
+          break;
+        default:
+          break;
+      }
+    }
+    return target;
+  }
+
+}
