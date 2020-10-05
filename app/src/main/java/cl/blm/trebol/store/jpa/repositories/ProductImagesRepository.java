f82092dabdab8239f79a125ae67e3d650c79f929
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-10-05 19:55:43 -0300

    add query method for product-to-many-imgs

--- app/src/main/java/cl/blm/trebol/store/jpa/repositories/ProductImagesRepository.java
+++ app/src/main/java/cl/blm/trebol/store/jpa/repositories/ProductImagesRepository.java
@@ -1,5 +1,7 @@
 package cl.blm.trebol.store.jpa.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -17,4 +19,7 @@ public interface ProductImagesRepository
 
   @Query("SELECT pi FROM ProductImage pi JOIN FETCH pi.image WHERE pi.product.id = :id")
   public Iterable<ProductImage> deepFindProductImagesByProductId(@Param("id") Integer id);
+
+  @Query("SELECT pi FROM ProductImage pi JOIN FETCH pi.image WHERE pi.product.id = :id ORDER BY pi.id LIMIT 1")
+  public Optional<ProductImage> deepFindFirstProductImageByProductId(@Param("id") Integer id);
 }
