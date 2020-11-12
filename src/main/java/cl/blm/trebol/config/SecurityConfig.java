d99a818746339da5e0c7f9e045552dd525f3597c
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-11-12 12:34:04 -0300

    correct which api routes are permitted to all

--- src/main/java/cl/blm/trebol/config/SecurityConfig.java
+++ src/main/java/cl/blm/trebol/config/SecurityConfig.java
@@ -62,7 +62,7 @@ public class SecurityConfig
             "/",
             "/login",
             "/company",
-            "/catalog/**"
+            "/store/**"
         ).permitAll()
         .anyRequest().authenticated();
   }
