4dad46f619783712edaecaed9ba4c7a8e66ba636
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-11-13 22:18:27 -0300

    create pojo for webpay redirection data

---
+++ src/main/java/cl/blm/trebol/api/pojo/WebPayRedirectionData.java
@@ -0,0 +1,36 @@
+package cl.blm.trebol.api.pojo;
+
+import javax.validation.constraints.NotEmpty;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+/**
+ *
+ * @author Benjamin La Madrid <bg.lamadrid@gmail.com>
+ */
+@JsonInclude
+public class WebPayRedirectionData {
+  @NotEmpty
+  private String url;
+  @NotEmpty
+  private String token_ws;
+
+  public WebPayRedirectionData() {
+  }
+
+  public String getUrl() {
+    return url;
+  }
+
+  public void setUrl(String url) {
+    this.url = url;
+  }
+
+  public String getToken_ws() {
+    return token_ws;
+  }
+
+  public void setToken_ws(String token_ws) {
+    this.token_ws = token_ws;
+  }
+}
