dd8b947951249ff5458febd8c724a08cdd828d18
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-11-14 00:12:14 -0300

    add UserDetails parameter to saveCartAsTransactionRequest

--- src/main/java/cl/blm/trebol/services/exposed/CheckoutService.java
+++ src/main/java/cl/blm/trebol/services/exposed/CheckoutService.java
@@ -2,6 +2,8 @@ package cl.blm.trebol.services.exposed;
 
 import java.util.Collection;
 
+import org.springframework.security.core.userdetails.UserDetails;
+
 import cl.blm.trebol.api.pojo.SellDetailPojo;
 import cl.blm.trebol.api.pojo.SellPojo;
 import cl.blm.trebol.api.pojo.WebPayRedirectionData;
@@ -11,7 +13,7 @@ import cl.blm.trebol.api.pojo.WebPayRedirectionData;
  * @author Benjamin La Madrid <bg.lamadrid@gmail.com>
  */
 public interface CheckoutService {
-  public SellPojo saveCartAsTransactionRequest(Collection<SellDetailPojo> cartDetails);
+  public SellPojo saveCartAsTransactionRequest(UserDetails clientUserDetails, Collection<SellDetailPojo> cartDetails);
   public WebPayRedirectionData startWebpayTransaction(SellPojo sellTransaction);
   public Integer confirmWebpayTransactionResult(String transactionToken);
 }
