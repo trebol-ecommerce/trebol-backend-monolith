f413f2ea14bd6b2bafa6cfe62ae510a770374cf7
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-11-13 22:56:46 -0300

    add checkout service

---
+++ src/main/java/cl/blm/trebol/services/exposed/CheckoutService.java
@@ -0,0 +1,17 @@
+package cl.blm.trebol.services.exposed;
+
+import java.util.Collection;
+
+import cl.blm.trebol.api.pojo.SellDetailPojo;
+import cl.blm.trebol.api.pojo.SellPojo;
+import cl.blm.trebol.api.pojo.WebPayRedirectionData;
+
+/**
+ *
+ * @author Benjamin La Madrid <bg.lamadrid@gmail.com>
+ */
+public interface CheckoutService {
+  public SellPojo saveCartAsTransactionRequest(Collection<SellDetailPojo> cartDetails);
+  public WebPayRedirectionData startWebpayTransaction(SellPojo sellTransaction);
+  public Integer confirmWebpayTransactionResult(String transactionToken);
+}
