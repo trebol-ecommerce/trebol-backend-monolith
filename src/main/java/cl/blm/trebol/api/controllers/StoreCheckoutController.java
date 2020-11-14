9135fee18900ea8b681226ecd3828ad6a262fee3
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-11-13 22:59:14 -0300

    implement controller flow for checking out

--- src/main/java/cl/blm/trebol/api/controllers/StoreCheckoutController.java
+++ src/main/java/cl/blm/trebol/api/controllers/StoreCheckoutController.java
@@ -2,12 +2,16 @@ package cl.blm.trebol.api.controllers;
 
 import java.util.Collection;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import cl.blm.trebol.api.pojo.SellDetailPojo;
+import cl.blm.trebol.api.pojo.SellPojo;
+import cl.blm.trebol.api.pojo.WebPayRedirectionData;
+import cl.blm.trebol.services.exposed.CheckoutService;
 
 /**
  *
@@ -17,10 +21,18 @@ import cl.blm.trebol.api.pojo.SellDetailPojo;
 @RequestMapping("/store/checkout")
 public class StoreCheckoutController {
 
-  // TODO Implement this
+  private final CheckoutService checkoutService;
+
+  @Autowired
+  public StoreCheckoutController(CheckoutService checkoutService) {
+    this.checkoutService = checkoutService;
+  }
+
   @PostMapping("")
-  public Object submitCart(@RequestBody Collection<SellDetailPojo> cartDetails) {
-    throw new UnsupportedOperationException("Method not implemented");
+  public WebPayRedirectionData submitCart(@RequestBody Collection<SellDetailPojo> cartDetails) {
+    SellPojo savedCartTransactionRequest = checkoutService.saveCartAsTransactionRequest(cartDetails);
+    WebPayRedirectionData transactionRedirect = checkoutService.startWebpayTransaction(savedCartTransactionRequest);
+    return transactionRedirect;
   }
 
   // TODO Implement this
