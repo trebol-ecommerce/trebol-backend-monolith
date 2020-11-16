7acea900c5c0c6edb307321438ce84ba2e39a0b8
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-11-16 19:56:57 -0300

    add nullability annotation to service interface method

--- src/main/java/cl/blm/trebol/services/user/ClientPersonRelationService.java
+++ src/main/java/cl/blm/trebol/services/user/ClientPersonRelationService.java
@@ -1,11 +1,15 @@
 package cl.blm.trebol.services.user;
 
+import javax.annotation.Nullable;
+
 import cl.blm.trebol.api.pojo.ClientPojo;
 
 /**
+ * Service interface to link Client entities to Person
  *
  * @author Benjamin La Madrid <bg.lamadrid@gmail.com>
  */
 public interface ClientPersonRelationService {
+  @Nullable
   ClientPojo getClientFromPersonId(int personId);
 }
