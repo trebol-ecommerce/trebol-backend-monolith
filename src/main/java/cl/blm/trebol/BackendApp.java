97019f9ad5e2250059f96379f60f656f58fc5d69
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-11-16 23:19:52 -0300

    configure jvm for changing httpclient requests origin

--- src/main/java/cl/blm/trebol/BackendApp.java
+++ src/main/java/cl/blm/trebol/BackendApp.java
@@ -15,6 +15,7 @@ public class BackendApp {
    * @param args
    */
   public static void main(String[] args) {
+    System.setProperty("sun.net.http.allowRestrictedHeaders", "true");
     SpringApplication.run(BackendApp.class, args);
   }
 
