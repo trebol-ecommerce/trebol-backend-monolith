c207badfd8a5b39108ad70a52728a6254e63fd34
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-11-20 15:50:00 -0300

    create cors mappings for v2 trébol api's

--- src/main/java/cl/blm/trebol/config/CorsConfig.java
+++ src/main/java/cl/blm/trebol/config/CorsConfig.java
@@ -15,11 +15,82 @@ public abstract class CorsConfig
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-// uncomment and adjust as required
-//    registry.addMapping("/**")
-//        .allowedOrigins("*")
-//        .allowedMethods("GET,POST,PUT,DELETE,OPTIONS")
-//        .exposedHeaders("Content-Type,X-Amz-Date,Authorization,X-Api-Key,x-requested-with");
+    this.addStoreCorsMappings(registry);
+    this.addSessionCorsMappings(registry);
+    this.addDataAccessCorsMappings(registry);
+    this.addDataManagementCorsMappings(registry);
   }
 
+  protected void addStoreCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/store/about")
+        .allowedOrigins("*")
+        .allowedMethods("GET,OPTIONS")
+        .allowedHeaders("Authorization");
+
+    registry.addMapping("/store/categories")
+        .allowedOrigins("*")
+        .allowedMethods("GET,OPTIONS")
+        .allowedHeaders("Authorization");
+
+    registry.addMapping("/store/categories/*")
+        .allowedOrigins("*")
+        .allowedMethods("GET,OPTIONS")
+        .allowedHeaders("Authorization");
+
+    registry.addMapping("/store/front")
+        .allowedOrigins("*")
+        .allowedMethods("GET,OPTIONS")
+        .allowedHeaders("Authorization");
+
+    registry.addMapping("/store/product/*")
+        .allowedOrigins("*")
+        .allowedMethods("GET,OPTIONS")
+        .allowedHeaders("Authorization");
+
+    registry.addMapping("/store/checkout")
+        .allowedOrigins("*")
+        .allowedMethods("POST,OPTIONS")
+        .allowedHeaders("Authorization");
+
+    registry.addMapping("/store/checkout/validate")
+        .allowedOrigins("*")
+        .allowedMethods("POST,OPTIONS")
+        .allowedHeaders("Authorization");
+  }
+
+  protected void addSessionCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/guest")
+        .allowedOrigins("*")
+        .allowedMethods("POST,OPTIONS")
+        .allowedHeaders("Authorization");
+
+    registry.addMapping("/login")
+        .allowedOrigins("*")
+        .allowedMethods("POST,OPTIONS")
+        .allowedHeaders("Authorization");
+
+    registry.addMapping("/profile")
+        .allowedOrigins("*")
+        .allowedMethods("GET,PUT,OPTIONS")
+        .allowedHeaders("Authorization");
+
+    registry.addMapping("/register")
+        .allowedOrigins("*")
+        .allowedMethods("POST,OPTIONS")
+        .allowedHeaders("Authorization");
+  }
+
+  protected void addDataAccessCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/access/*")
+        .allowedOrigins("*")
+        .allowedMethods("GET,OPTIONS")
+        .allowedHeaders("Authorization");
+  }
+
+  private void addDataManagementCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/data/**")
+        .allowedOrigins("*")
+        .allowedMethods("GET,POST,PUT,DELETE,OPTIONS")
+        .allowedHeaders("Authorization");
+  }
 }
