35a428c3abba903aa123b1aeb9a23b26b0b7d7be
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-11-23 11:52:31 -0300

    update checkout service interface - should use WebpayTransactionPojo

--- src/main/java/cl/blm/trebol/services/exposed/CheckoutService.java
+++ src/main/java/cl/blm/trebol/services/exposed/CheckoutService.java
@@ -3,15 +3,15 @@ package cl.blm.trebol.services.exposed;
 import java.util.Collection;
 
 import cl.blm.trebol.api.pojo.SellDetailPojo;
-import cl.blm.trebol.api.pojo.SellPojo;
 import cl.blm.trebol.api.pojo.WebPayRedirectionData;
+import cl.blm.trebol.api.pojo.WebpayTransactionPojo;
 
 /**
  *
  * @author Benjamin La Madrid <bg.lamadrid@gmail.com>
  */
 public interface CheckoutService {
-  public SellPojo saveCartAsTransactionRequest(Integer clientId, Collection<SellDetailPojo> cartDetails);
-  public WebPayRedirectionData startWebpayTransaction(SellPojo sellTransaction);
+  public WebpayTransactionPojo saveCartAsTransactionRequest(Integer clientId, Collection<SellDetailPojo> cartDetails);
+  public WebPayRedirectionData startWebpayTransaction(WebpayTransactionPojo sellTransaction);
   public Integer confirmWebpayTransactionResult(String transactionToken);
 }
