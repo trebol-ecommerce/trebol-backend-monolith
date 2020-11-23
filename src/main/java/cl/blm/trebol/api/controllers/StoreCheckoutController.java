e769b442e2834e176cf80d299a9d503e566bee54
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-11-23 12:10:00 -0300

    update checkout controller to meet new service interface specs

--- src/main/java/cl/blm/trebol/api/controllers/StoreCheckoutController.java
+++ src/main/java/cl/blm/trebol/api/controllers/StoreCheckoutController.java
@@ -15,8 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 import cl.blm.trebol.api.pojo.ClientPojo;
 import cl.blm.trebol.api.pojo.PersonPojo;
 import cl.blm.trebol.api.pojo.SellDetailPojo;
-import cl.blm.trebol.api.pojo.SellPojo;
 import cl.blm.trebol.api.pojo.WebPayRedirectionData;
+import cl.blm.trebol.api.pojo.WebpayTransactionPojo;
 import cl.blm.trebol.services.exposed.CheckoutService;
 import cl.blm.trebol.services.security.AuthenticatedPeopleService;
 import cl.blm.trebol.services.security.AuthorizationHeaderParserService;
@@ -32,8 +32,6 @@ public class StoreCheckoutController {
 
   private final CheckoutService checkoutService;
   private final AuthorizationHeaderParserService<Claims> jwtClaimsParserService;
-  private final AuthenticatedPeopleService authenticatedPeopleService;
-  private final ClientPersonRelationService clientPersonRelationService;
 
   @Autowired
   public StoreCheckoutController(CheckoutService checkoutService,
@@ -42,25 +40,14 @@ public class StoreCheckoutController {
       ClientPersonRelationService clientPersonRelationService) {
     this.checkoutService = checkoutService;
     this.jwtClaimsParserService = jwtClaimsParserService;
-    this.authenticatedPeopleService = authenticatedPeopleService;
-    this.clientPersonRelationService = clientPersonRelationService;
-  }
-
-  private int fetchClientId(HttpHeaders httpHeaders) {
-    String authorizationHeader = jwtClaimsParserService.extractAuthorizationHeader(httpHeaders);
-    PersonPojo authenticatedPerson = authenticatedPeopleService.fetchAuthenticatedUserPersonProfile(authorizationHeader);
-    int personId = authenticatedPerson.getId();
-    ClientPojo authenticatedClient = clientPersonRelationService.getClientFromPersonId(personId);
-    int clientId = authenticatedClient.getId();
-    return clientId;
   }
 
   @PostMapping("")
   public WebPayRedirectionData submitCart(
       @RequestHeader HttpHeaders httpHeaders,
       @RequestBody Collection<SellDetailPojo> cartDetails) {
-    int clientId = fetchClientId(httpHeaders);
-    SellPojo savedCartTransactionRequest = checkoutService.saveCartAsTransactionRequest(clientId, cartDetails);
+    String authorization = jwtClaimsParserService.extractAuthorizationHeader(httpHeaders);
+    WebpayTransactionPojo savedCartTransactionRequest = checkoutService.saveCartAsTransactionRequest(authorization, cartDetails);
     WebPayRedirectionData transactionRedirect = checkoutService.startWebpayTransaction(savedCartTransactionRequest);
     return transactionRedirect;
   }
