6129f5d91d5f6fa7cca9f4ca575553d2b71a1c9f
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-12-10 22:04:36 -0300

    remove autowired bean provided in same class

--- src/main/java/cl/blm/trebol/config/SecurityConfig.java
+++ src/main/java/cl/blm/trebol/config/SecurityConfig.java
@@ -37,7 +37,6 @@ public class SecurityConfig
 
   @Autowired
   public SecurityConfig(
-      PasswordEncoder passwordEncoder,
       UserDetailsService userDetailsService,
       SecretKey secretKey,
       SecurityProperties securityProperties,
