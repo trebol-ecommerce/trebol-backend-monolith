ef9a6425e54ef24a4745670e2c8e8e118a5f4aac
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-12-11 22:13:58 -0300

    use RegistrationPojo

--- src/main/java/cl/blm/trebol/api/controllers/RegistrationController.java
+++ src/main/java/cl/blm/trebol/api/controllers/RegistrationController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import cl.blm.trebol.api.pojo.RegistrationPojo;
-import cl.blm.trebol.api.pojo.UserPojo;
 
 /**
  *
