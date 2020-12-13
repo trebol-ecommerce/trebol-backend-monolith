545c4a279600b0e948791502ff3ee10210c5f9a4
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-12-12 21:42:53 -0300

    wire registration service into controller

--- src/main/java/cl/blm/trebol/api/controllers/RegistrationController.java
+++ src/main/java/cl/blm/trebol/api/controllers/RegistrationController.java
@@ -1,11 +1,13 @@
 package cl.blm.trebol.api.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import cl.blm.trebol.api.pojo.RegistrationPojo;
+import cl.blm.trebol.services.exposed.RegistrationService;
 
 /**
  *
@@ -15,8 +17,15 @@ import cl.blm.trebol.api.pojo.RegistrationPojo;
 @RequestMapping("/register")
 public class RegistrationController {
 
+  private final RegistrationService registrationService;
+
+  @Autowired
+  public RegistrationController(RegistrationService registrationService) {
+    this.registrationService = registrationService;
+  }
+
   @PostMapping
   public boolean register(@RequestBody RegistrationPojo userProfile) {
-    throw new UnsupportedOperationException();
+    return this.registrationService.register(userProfile);
   }
 }
