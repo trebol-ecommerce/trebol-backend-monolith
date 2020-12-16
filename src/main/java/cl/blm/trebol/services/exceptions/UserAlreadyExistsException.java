6ae1ac95dc1a3650788c1b928088d149975c4ae1
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-12-16 00:51:15 -0300

    add new custom exceptions

---
+++ src/main/java/cl/blm/trebol/services/exceptions/UserAlreadyExistsException.java
@@ -0,0 +1,24 @@
+package cl.blm.trebol.services.exceptions;
+
+/**
+ *
+ * @author Benjamin La Madrid <bg.lamadrid@gmail.com>
+ */
+public class UserAlreadyExistsException
+  extends Exception {
+
+  public UserAlreadyExistsException() {
+  }
+
+  public UserAlreadyExistsException(String string) {
+    super(string);
+  }
+
+  public UserAlreadyExistsException(String string, Throwable thrwbl) {
+    super(string, thrwbl);
+  }
+
+  public UserAlreadyExistsException(Throwable thrwbl) {
+    super(thrwbl);
+  }
+}
