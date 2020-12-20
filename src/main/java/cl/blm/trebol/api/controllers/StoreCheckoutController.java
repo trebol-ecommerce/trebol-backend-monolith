e3887ae8a607abd718389ce2d7f626da5e5507ed
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-12-19 23:41:08 -0300

    use checkoutconfig to redirect at controller level

--- src/main/java/cl/blm/trebol/api/controllers/StoreCheckoutController.java
+++ src/main/java/cl/blm/trebol/api/controllers/StoreCheckoutController.java
@@ -1,7 +1,6 @@
 package cl.blm.trebol.api.controllers;
 
 import java.util.Collection;
-import java.util.Map;
 
 import io.jsonwebtoken.Claims;
 
@@ -9,10 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import cl.blm.trebol.api.pojo.ClientPojo;
@@ -20,10 +21,10 @@ import cl.blm.trebol.api.pojo.PersonPojo;
 import cl.blm.trebol.api.pojo.SellDetailPojo;
 import cl.blm.trebol.api.pojo.WebPayRedirectionData;
 import cl.blm.trebol.api.pojo.WebpayTransactionPojo;
+import cl.blm.trebol.config.CheckoutConfig;
 import cl.blm.trebol.services.exposed.CheckoutService;
 import cl.blm.trebol.services.security.AuthenticatedPeopleService;
 import cl.blm.trebol.services.security.AuthorizationHeaderParserService;
-import cl.blm.trebol.services.user.ClientPersonRelationService;
 
 /**
  *
@@ -35,14 +36,16 @@ public class StoreCheckoutController {
 
   private final CheckoutService checkoutService;
   private final AuthorizationHeaderParserService<Claims> jwtClaimsParserService;
+  private final CheckoutConfig checkoutConfig;
 
   @Autowired
-  public StoreCheckoutController(CheckoutService checkoutService,
+  public StoreCheckoutController(
+      CheckoutService checkoutService,
       AuthorizationHeaderParserService<Claims> jwtClaimsParserService,
-      AuthenticatedPeopleService authenticatedPeopleService,
-      ClientPersonRelationService clientPersonRelationService) {
+      CheckoutConfig checkoutConfig) {
     this.checkoutService = checkoutService;
     this.jwtClaimsParserService = jwtClaimsParserService;
+    this.checkoutConfig = checkoutConfig;
   }
 
   @PostMapping("")
@@ -56,9 +59,15 @@ public class StoreCheckoutController {
   }
 
   @PostMapping("/validate")
-  public ResponseEntity<Void> validateTransaction(@RequestBody Map<String, String> transactionFormData) {
-//    TODO: formData should contain a token_ws that can be matched to a saved database transaction
-//    ResponseEntity<Void> response = ResponseEntity.status(HttpStatus.SEE_OTHER).header("Location", headerValues)
-    return ResponseEntity.ok().build();
+  public ResponseEntity<Void> validateTransaction(@RequestBody MultiValueMap<String, String> transactionFormData) {
+    String tokenWs = transactionFormData.getFirst(checkoutConfig.getTransactionTokenPostDataKey());
+    String returnUrl;
+    try {
+      checkoutService.confirmWebpayTransactionResult(tokenWs);
+      returnUrl = checkoutConfig.getSuccessPageURL();
+    } catch (Exception exc) {
+      returnUrl = checkoutConfig.getFailurePageURL();
+    }
+    return ResponseEntity.status(HttpStatus.SEE_OTHER).header("Location", returnUrl).build();
   }
 }
