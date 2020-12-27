85e15810353b8879dc0a923e044614e633a0d4f8
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-12-27 16:52:11 -0300

    wire receiptservice

--- src/main/java/cl/blm/trebol/api/controllers/StoreReceiptController.java
+++ src/main/java/cl/blm/trebol/api/controllers/StoreReceiptController.java
@@ -3,12 +3,14 @@ package cl.blm.trebol.api.controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import cl.blm.trebol.api.pojo.ReceiptPojo;
+import cl.blm.trebol.services.exposed.ReceiptService;
 
 /**
  *
@@ -19,13 +21,18 @@ import cl.blm.trebol.api.pojo.ReceiptPojo;
 public class StoreReceiptController {
   private final Logger LOG = LoggerFactory.getLogger(StoreReceiptController.class);
 
-//  private final ReceiptService receiptService;
+  private final ReceiptService receiptService;
 
-  public StoreReceiptController() {
+  @Autowired
+  public StoreReceiptController(ReceiptService receiptService) {
+    this.receiptService = receiptService;
   }
 
   @GetMapping("/{id}")
   public ReceiptPojo fetchReceiptById(@PathVariable("id") Integer id) {
-    throw new UnsupportedOperationException("Not implemented");
+    if (id == null) {
+      throw new RuntimeException("An incorrect receipt id was provided");
+    }
+    return this.receiptService.fetchReceiptById(id);
   }
 }
