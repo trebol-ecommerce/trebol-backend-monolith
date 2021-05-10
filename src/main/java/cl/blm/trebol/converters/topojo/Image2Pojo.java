38966df440df3fa8b5bb00073916efad9b5510e3
Benjamin La Madrid <bg.lamadrid@gmail.com>
2021-05-10 18:51:29 -0400

    register converter as spring component

--- src/main/java/cl/blm/trebol/converters/topojo/Image2Pojo.java
+++ src/main/java/cl/blm/trebol/converters/topojo/Image2Pojo.java
@@ -1,6 +1,7 @@
 package cl.blm.trebol.converters.topojo;
 
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
 
 import cl.blm.trebol.api.pojo.ImagePojo;
 import cl.blm.trebol.jpa.entities.Image;
@@ -9,6 +10,7 @@ import cl.blm.trebol.jpa.entities.Image;
  *
  * @author Benjamin La Madrid <bg.lamadrid at gmail.com>
  */
+@Component
 public class Image2Pojo
     implements Converter<Image, ImagePojo> {
 
