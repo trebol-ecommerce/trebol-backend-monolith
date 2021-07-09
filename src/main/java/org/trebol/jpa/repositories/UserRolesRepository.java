0a3b2e77fdde61f79b352730281ff95faa5a8873
Benjamin La Madrid <bg.lamadrid@gmail.com>
2021-07-08 21:35:38 -0400

    add query to find user role by string name

--- src/main/java/org/trebol/jpa/repositories/UserRolesRepository.java
+++ src/main/java/org/trebol/jpa/repositories/UserRolesRepository.java
@@ -1,5 +1,8 @@
 package org.trebol.jpa.repositories;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import org.trebol.jpa.GenericRepository;
@@ -12,5 +15,8 @@ import org.trebol.jpa.entities.UserRole;
 @Repository
 public interface UserRolesRepository
     extends GenericRepository<UserRole, Integer> {
+
+  @Query
+  Optional<UserRole> findByName(String name);
 
 }
