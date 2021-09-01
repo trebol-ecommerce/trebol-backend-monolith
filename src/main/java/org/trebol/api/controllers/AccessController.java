8860c25631b12a29b4af1266d339c4733de0621f
Benjamin La Madrid <bg.lamadrid@gmail.com>
2021-09-01 17:57:20 -0400

    fix access controller not parsing tokens correctly
    also, throw 401 errors for specific exceptions in that controller

--- src/main/java/org/trebol/api/controllers/AccessController.java
+++ src/main/java/org/trebol/api/controllers/AccessController.java
@@ -9,15 +9,19 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.trebol.api.pojo.AuthorizedAccessPojo;
 import org.trebol.security.IAuthorizedApiService;
 import org.trebol.security.IAuthorizationHeaderParserService;
+
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 @RestController
 @RequestMapping("/access")
@@ -29,49 +33,62 @@ public class AccessController {
 
   @Autowired
   public AccessController(IAuthorizationHeaderParserService<Claims> jwtClaimsParserService,
-      UserDetailsService userDetailsService,
-      IAuthorizedApiService routeService) {
+    UserDetailsService userDetailsService, IAuthorizedApiService routeService) {
     this.jwtClaimsParserService = jwtClaimsParserService;
     this.userDetailsService = userDetailsService;
     this.routeService = routeService;
   }
 
-  private UserDetails getUserDetails(String authorizationHeader) throws UsernameNotFoundException {
-    Claims body = jwtClaimsParserService.parseToken(authorizationHeader);
-    String username = body.getSubject();
-    UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-    return userDetails;
-  }
-
-  private AuthorizedAccessPojo loadApiRouteAccess(String authorizationHeader, String apiRoute) throws UsernameNotFoundException {
-    UserDetails userDetails = getUserDetails(authorizationHeader);
-    Collection<String> permissions = routeService.getAuthorizedApiRouteAccess(userDetails, apiRoute);
-    AuthorizedAccessPojo target = new AuthorizedAccessPojo();
-    target.setPermissions(permissions);
-    return target;
+  private UserDetails getUserDetails(HttpHeaders requestHeaders)
+    throws UsernameNotFoundException, IllegalStateException {
+    String authorizationHeader = jwtClaimsParserService.extractAuthorizationHeader(requestHeaders);
+    if (authorizationHeader == null || !authorizationHeader.matches("^Bearer .+$")) {
+      return null;
+    } else {
+      String token = authorizationHeader.replace("Bearer ", "");
+      Claims body = jwtClaimsParserService.parseToken(token);
+      String username = body.getSubject();
+      UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+      return userDetails;
+    }
   }
 
   @GetMapping({"", "/"})
-  public AuthorizedAccessPojo getApiRoutesAccess(@RequestHeader HttpHeaders requestHeaders) {
-    String authorizationHeader = jwtClaimsParserService.extractAuthorizationHeader(requestHeaders);
-    if (authorizationHeader != null) {
-      UserDetails userDetails = getUserDetails(authorizationHeader);
+  public AuthorizedAccessPojo getApiRoutesAccess(@RequestHeader HttpHeaders requestHeaders)
+    throws UsernameNotFoundException, IllegalStateException {
+    UserDetails userDetails = this.getUserDetails(requestHeaders);
+    if (userDetails == null) {
+      return null;
+    } else {
       Collection<String> routes = routeService.getAuthorizedApiRoutes(userDetails);
       AuthorizedAccessPojo target = new AuthorizedAccessPojo();
       target.setRoutes(routes);
       return target;
     }
-    return null;
   }
 
   @GetMapping({"/{apiRoute}", "/{apiRoute}/"})
-  public AuthorizedAccessPojo getApiResourceAccess(@RequestHeader HttpHeaders requestHeaders,
-      @PathVariable String apiRoute) {
-    String authorizationHeader = jwtClaimsParserService.extractAuthorizationHeader(requestHeaders);
-    if (authorizationHeader != null) {
-      AuthorizedAccessPojo target = loadApiRouteAccess(authorizationHeader, apiRoute);
+  public AuthorizedAccessPojo getApiResourceAccess(
+      @RequestHeader HttpHeaders requestHeaders,
+      @PathVariable String apiRoute)
+    throws UsernameNotFoundException, IllegalStateException {
+    UserDetails userDetails = this.getUserDetails(requestHeaders);
+    if (userDetails == null) {
+      return null;
+    } else {
+      Collection<String> permissions = routeService.getAuthorizedApiRouteAccess(userDetails, apiRoute);
+      AuthorizedAccessPojo target = new AuthorizedAccessPojo();
+      target.setPermissions(permissions);
       return target;
     }
-    return null;
   }
+
+  @ResponseStatus(UNAUTHORIZED)
+  @ExceptionHandler(UsernameNotFoundException.class)
+  public void handleException(UsernameNotFoundException ex) { }
+
+  @ResponseStatus(UNAUTHORIZED)
+  @ExceptionHandler(IllegalStateException.class)
+  public void handleException(IllegalStateException ex) { }
+  
 }
