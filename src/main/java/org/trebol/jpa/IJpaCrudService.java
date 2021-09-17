df2b3fe541ed99f8010deef116b1afbfe61fcd28
Benjamin La Madrid <bg.lamadrid@gmail.com>
2021-09-17 04:01:59 -0300

    remove ICrudService, just keep IJpaCrudService

--- src/main/java/org/trebol/jpa/IJpaCrudService.java
+++ src/main/java/org/trebol/jpa/IJpaCrudService.java
@@ -1,6 +1,8 @@
 package org.trebol.jpa;
 
 import org.springframework.lang.Nullable;
+import org.trebol.exceptions.BadInputException;
+import org.trebol.exceptions.EntityAlreadyExistsException;
 import org.trebol.pojo.DataPagePojo;
 
 import javassist.NotFoundException;
@@ -15,7 +17,19 @@ import javassist.NotFoundException;
  * @param <F> The filter type class.
  */
 public interface IJpaCrudService<T, I, F>
-  extends ICrudService<T, I>, IQueryDslPredicateParserService {
+  extends IQueryDslPredicateParserService {
+
+  /**
+   * Inserts and persists an item.
+   *
+   * @param dto The item to be created.
+   *
+   * @return The created item, with updated properties (most importantly its ID),
+   *         or null if the item could not be created.
+   * @throws org.trebol.exceptions.BadInputException
+   * @throws org.trebol.exceptions.EntityAlreadyExistsException
+   */
+  T create(T dto) throws BadInputException, EntityAlreadyExistsException;
 
   /**
    * Queries a paged collection of items.
@@ -37,4 +51,37 @@ public interface IJpaCrudService<T, I, F>
    * @throws javassist.NotFoundException
    */
   T readOne(F filters) throws NotFoundException;
+
+  /**
+   * Retrieves an item by its ID.
+   *
+   * @param id The unique identifier of the item.
+   *
+   * @return The requested item.
+   * @throws javassist.NotFoundException
+   */
+  T readOne(Long id) throws NotFoundException;
+
+  /**
+   * Updates an existing item.
+   *
+   * @param dto The item to be updated. Its identifying field may or may not be
+   *            present, and can be different from the second method param.
+   * @param id  The unique identifier of the item.
+   *
+   * @return The saved item, with updated properties, or null if the item was not
+   *         found.
+   * @throws javassist.NotFoundException
+   * @throws org.trebol.exceptions.BadInputException
+   */
+  T update(T dto, I id) throws NotFoundException, BadInputException;
+
+  /**
+   * Finds an item by its ID and deletes it.
+   *
+   * @param id Its unique identifier.
+   *
+   * @throws javassist.NotFoundException
+   */
+  void delete(I id) throws NotFoundException;
 }
