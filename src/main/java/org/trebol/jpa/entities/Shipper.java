d0a21fc8034265fda0cfd5599d014c4414c16c88
Benjamin La Madrid <bg.lamadrid@gmail.com>
2021-11-24 14:07:57 -0300

    add shipper jpa crud service

--- src/main/java/org/trebol/jpa/entities/Shipper.java
+++ src/main/java/org/trebol/jpa/entities/Shipper.java
@@ -36,6 +36,11 @@ public class Shipper
 
   public Shipper() { }
 
+  public Shipper(Shipper source) {
+    this.id = source.id;
+    this.name = source.name;
+  }
+
   public Long getId() {
     return id;
   }
