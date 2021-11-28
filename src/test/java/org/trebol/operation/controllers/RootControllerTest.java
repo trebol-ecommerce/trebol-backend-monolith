aa8d2632df1ec286a920d1607a8294cbe89a080b
Benjamin La Madrid <bg.lamadrid@gmail.com>
2021-11-28 13:21:54 -0300

    add unit test for calling GET /

---
+++ src/test/java/org/trebol/operation/controllers/RootControllerTest.java
@@ -0,0 +1,26 @@
+package org.trebol.operation.controllers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.*;
+
+@RunWith(SpringRunner.class)
+@WebAppConfiguration
+public class RootControllerTest {
+
+  @Test
+  public void return_200OK() {
+    given().
+      standaloneSetup(new RootController()).
+    when().
+      get("/").
+    then().
+      status(HttpStatus.OK);
+  }
+
+}
