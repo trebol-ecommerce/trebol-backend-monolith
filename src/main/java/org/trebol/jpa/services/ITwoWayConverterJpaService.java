609bf998a50ec35d8a811f1c8a4e72cd3b0bffd9
Benjamin La Madrid <bg.lamadrid@gmail.com>
2021-11-30 16:54:57 -0300

    rewrite some ITwoWayConverterJpaService javadoc for clarity

--- src/main/java/org/trebol/jpa/services/ITwoWayConverterJpaService.java
+++ src/main/java/org/trebol/jpa/services/ITwoWayConverterJpaService.java
@@ -15,8 +15,8 @@ import javax.annotation.Nullable;
 public interface ITwoWayConverterJpaService<P, E> {
 
   /**
-   * Straightly converts a Pojo to a new entity, assuming that the Pojo is already @Valid.
-   * This method DOES NOT persist said entity.
+   * Straightly converts a Pojo to a new @Entity, assuming that the Pojo is already @Valid.
+   * This method DOES NOT persist data.
    * @param source The source Pojo.
    * @return A new entity, prepared to be saved to the database.
    * @throws BadInputException If the source object does not include required data or has invalid values
@@ -24,18 +24,20 @@ public interface ITwoWayConverterJpaService<P, E> {
   E convertToNewEntity(P source) throws BadInputException;
 
   /**
-   * Creates a clone entity, then updates it with new data from a Pojo class, putting differences
-   * property-by-property. This method DOES NOT persist the aforementioned entity.
+   * Creates a clone @Entity, then updates it with new data from a Pojo class,
+   * putting differences in properties one-by-one. It does not include relationships to other entities.
+   * This method DOES NOT persist data.
    * @param source The Pojo containing data updates.
    * @param target The target entity.
+   * @return An updated instance of the @Entity, prepared to be saved to the database.
    * @throws BadInputException If the object with changes has invalid values
    */
   E applyChangesToExistingEntity(P source, E target) throws BadInputException;
 
   /**
-   * Converts an existing Entity to its Pojo equivalent.
-   * @param source The source entity.
-   * @return The resulting Pojo.
+   * Converts an existing @Entity to its complete Pojo equivalent, including all relationships to other entities.
+   * @param source The source @Entity.
+   * @return The resulting Pojo, or null if the @Entity isn't persistent
    */
   @Nullable
   P convertToPojo(E source);
