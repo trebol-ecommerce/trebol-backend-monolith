b5b53c8b0dfdfb4c76dd02644f2329526e80346b
Benjamin La Madrid <bg.lamadrid@gmail.com>
2021-12-03 21:06:15 -0300

    add unit tests for GenericJpaCrudService

---
+++ src/test/java/org/trebol/jpa/services/GenericEntity.java
@@ -0,0 +1,11 @@
+package org.trebol.jpa.services;
+
+public class GenericEntity {
+  private Long id;
+  private String name;
+
+  public GenericEntity(Long id, String name) {
+    this.id = id;
+    this.name = name;
+  }
+}
