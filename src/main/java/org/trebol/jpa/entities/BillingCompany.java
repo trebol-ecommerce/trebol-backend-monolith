305d2a7e165696f0ef823aa1edf9f5b28f1ae36a
Benjamin La Madrid <bg.lamadrid@gmail.com>
2021-12-04 19:07:13 -0300

    reorder billing company classes fields
    id number goes before name
    generate methods equals, hashCode, toString using intellij idea

--- src/main/java/org/trebol/jpa/entities/BillingCompany.java
+++ src/main/java/org/trebol/jpa/entities/BillingCompany.java
@@ -1,20 +1,9 @@
 package org.trebol.jpa.entities;
 
+import javax.persistence.*;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 import java.util.Objects;
-
-import javax.persistence.Basic;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 /**
  *
@@ -31,19 +20,19 @@ public class BillingCompany
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "billing_company_id", nullable = false)
   private Long id;
-  @Size(min = 1, max = 100)
-  @Column(name = "billing_company_name", nullable = false, unique = true)
-  private String name;
   @Size(min = 1, max = 20)
   @Column(name = "billing_company_id_number", nullable = false, unique = true)
   private String idNumber;
+  @Size(min = 1, max = 100)
+  @Column(name = "billing_company_name", nullable = false, unique = true)
+  private String name;
 
   public BillingCompany() { }
 
   public BillingCompany(BillingCompany source) {
     this.id = source.id;
-    this.name = source.name;
     this.idNumber = source.idNumber;
+    this.name = source.name;
   }
 
   public Long getId() {
@@ -54,14 +43,6 @@ public class BillingCompany
     this.id = id;
   }
 
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
   public String getIdNumber() {
     return idNumber;
   }
@@ -70,27 +51,35 @@ public class BillingCompany
     this.idNumber = idNumber;
   }
 
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
     BillingCompany that = (BillingCompany) o;
     return Objects.equals(id, that.id) &&
-        Objects.equals(name, that.name) &&
-        Objects.equals(idNumber, that.idNumber);
+        Objects.equals(idNumber, that.idNumber) &&
+        Objects.equals(name, that.name);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(id, name, idNumber);
+    return Objects.hash(id, idNumber, name);
   }
 
   @Override
   public String toString() {
     return "BillingCompany{" +
         "id=" + id +
-        ", name='" + name + '\'' +
         ", idNumber='" + idNumber + '\'' +
+        ", name='" + name + '\'' +
         '}';
   }
 }
