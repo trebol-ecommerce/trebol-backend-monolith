839c6883295e880a1f409f49cedd141b72e7b767
Benjamin La Madrid <bg.lamadrid@gmail.com>
2022-01-07 02:22:01 -0300

    build: update webpay sdk dependency to v3
    update sdk api usage

--- src/main/java/org/trebol/integration/payments/webpayplus/WebpayplusPaymentServiceImpl.java
+++ src/main/java/org/trebol/integration/payments/webpayplus/WebpayplusPaymentServiceImpl.java
@@ -1,10 +1,13 @@
 package org.trebol.integration.payments.webpayplus;
 
+import cl.transbank.common.IntegrationApiKeys;
+import cl.transbank.common.IntegrationCommerceCodes;
 import cl.transbank.common.IntegrationType;
+import cl.transbank.webpay.common.WebpayOptions;
 import cl.transbank.webpay.exception.TransactionCommitException;
 import cl.transbank.webpay.exception.TransactionCreateException;
 import cl.transbank.webpay.webpayplus.WebpayPlus;
-import cl.transbank.webpay.webpayplus.model.WebpayPlusTransactionCreateResponse;
+import cl.transbank.webpay.webpayplus.responses.WebpayPlusTransactionCreateResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,10 +19,6 @@ import org.trebol.pojo.SellPojo;
 
 import java.io.IOException;
 
-/**
- *
- * @author Benjamin La Madrid <bg.lamadrid at gmail.com>
- */
 @Service
 public class WebpayplusPaymentServiceImpl
   implements IPaymentsIntegrationService {
@@ -35,19 +34,16 @@ public class WebpayplusPaymentServiceImpl
   @Override
   public PaymentRedirectionDetailsPojo requestNewPaymentPageDetails(SellPojo transaction) throws PaymentServiceException {
 
-    if (properties.isProduction()) {
-      WebpayPlus.Transaction.setCommerceCode(properties.getCommerceCode());
-      WebpayPlus.Transaction.setApiKey(properties.getApiKey());
-      WebpayPlus.Transaction.setIntegrationType(IntegrationType.LIVE);
-    }
 
     String buyOrder = transaction.getBuyOrder().toString();
     String sessionId = String.valueOf(transaction.hashCode());
     double amount = transaction.getNetValue();
     String returnUrl = properties.getCallbackUrl();
 
+    WebpayPlus.Transaction webpayTransaction = this.createWebpayTransaction();
+
     try {
-      WebpayPlusTransactionCreateResponse webpayResponse = WebpayPlus.Transaction.create(
+      WebpayPlusTransactionCreateResponse webpayResponse = webpayTransaction.create(
         buyOrder, sessionId, amount, returnUrl
       );
       PaymentRedirectionDetailsPojo response = new PaymentRedirectionDetailsPojo();
@@ -62,8 +58,9 @@ public class WebpayplusPaymentServiceImpl
 
   @Override
   public int requestPaymentResult(String transactionToken) throws PaymentServiceException {
+    WebpayPlus.Transaction webpayTransaction = this.createWebpayTransaction();
     try {
-      return WebpayPlus.Transaction.commit(transactionToken).getResponseCode();
+      return webpayTransaction.commit(transactionToken).getResponseCode();
     } catch (TransactionCommitException exc) {
       return 1;
     } catch (IOException exc) {
@@ -75,6 +72,21 @@ public class WebpayplusPaymentServiceImpl
   @Override
   public String getPaymentResultPageUrl() {
     return properties.getBrowserRedirectionUrl();
+  }
+
+  private WebpayPlus.Transaction createWebpayTransaction() {
+    String commerceCode = IntegrationCommerceCodes.WEBPAY_PLUS;
+    String apiKey = IntegrationApiKeys.WEBPAY;
+    IntegrationType integrationType = IntegrationType.MOCK;
+    if (properties.isProduction()) {
+      commerceCode = properties.getCommerceCode();
+      apiKey = properties.getApiKey();
+      integrationType = IntegrationType.LIVE;
+    }
+
+    WebpayOptions wpOptions = new WebpayOptions(commerceCode, apiKey, integrationType);
+
+    return new WebpayPlus.Transaction(wpOptions);
   }
 
 }
