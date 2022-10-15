44344685e31146f7606d696002a24895ee4128d3
Nyor Ja <botsot.felix@gmail.com>
2022-10-15 12:40:41 +0800

    Optimize imports

--- src/test/java/org/trebol/operation/services/CheckoutServiceTest.java
+++ src/test/java/org/trebol/operation/services/CheckoutServiceTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.trebol.constant.TestConstants;
 import org.trebol.exceptions.BadInputException;
 import org.trebol.integration.IPaymentsIntegrationService;
 import org.trebol.integration.exceptions.PaymentServiceException;
