6ee2a467dbc6b2b0843413fff0b7718fb9d62025
mepox <21198248+mepox@users.noreply.github.com>
2022-10-23 16:25:19 +0100

    test: add more unit test for SalesProcessServiceImpl

--- src/test/java/org/trebol/operation/services/SalesProcessServiceImplTest.java
+++ src/test/java/org/trebol/operation/services/SalesProcessServiceImplTest.java
@@ -14,17 +14,21 @@ import static org.trebol.config.Constants.SELL_STATUS_PAID_CONFIRMED;
 import static org.trebol.config.Constants.SELL_STATUS_REJECTED;
 import static org.trebol.config.Constants.SELL_STATUS_COMPLETED;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.trebol.exceptions.BadInputException;
 import org.trebol.jpa.entities.Product;
 import org.trebol.jpa.entities.Sell;
+import org.trebol.jpa.entities.SellDetail;
 import org.trebol.jpa.entities.SellStatus;
 import org.trebol.jpa.repositories.ISalesJpaRepository;
 import org.trebol.jpa.repositories.ISellDetailsJpaRepository;
@@ -32,19 +36,30 @@ import org.trebol.jpa.repositories.ISellStatusesJpaRepository;
 import org.trebol.jpa.services.GenericCrudJpaService;
 import org.trebol.jpa.services.ITwoWayConverterJpaService;
 import org.trebol.pojo.ProductPojo;
+import org.trebol.pojo.SellDetailPojo;
 import org.trebol.pojo.SellPojo;
+import org.trebol.testhelpers.ProductsTestHelper;
 
 @ExtendWith(MockitoExtension.class)
 class SalesProcessServiceImplTest {
 	
-	@InjectMocks	SalesProcessServiceImpl instance;
 	@Mock	GenericCrudJpaService<SellPojo, Sell> crudService;
 	@Mock	ISalesJpaRepository salesRepository;
-	@Mock	ITwoWayConverterJpaService<SellPojo, Sell> converterService;	
 	@Mock	ISellStatusesJpaRepository sellStatusesRepository;
+	@Mock	ISellDetailsJpaRepository sellDetailsRepository;
 	
-	@Mock	ISellDetailsJpaRepository sellDetailsRepository;	
-	//@Mock	ProductsConverterJpaServiceImpl productConverterService;
+	ITwoWayConverterJpaService<SellPojo, Sell> converterService;
+	ITwoWayConverterJpaService<ProductPojo, Product> productConverterService;
+	SalesProcessServiceImpl instance;
+	
+	@BeforeEach
+	void beforeEach() {
+		converterService = (ITwoWayConverterJpaService<SellPojo, Sell>) Mockito.mock(ITwoWayConverterJpaService.class);
+		productConverterService = (ITwoWayConverterJpaService<ProductPojo, Product>) 
+				Mockito.mock(ITwoWayConverterJpaService.class);
+		instance = new SalesProcessServiceImpl(crudService, salesRepository, sellDetailsRepository, 
+				sellStatusesRepository, converterService, productConverterService);
+	}
 	
 	@Nested
 	class MarkAsStarted {	
@@ -275,7 +290,43 @@ class SalesProcessServiceImplTest {
 			when(converterService.convertToPojo(any())).thenReturn(sellPojoMock); // convertOrThrowException
 					
 			assertEquals(SELL_STATUS_PAID_UNCONFIRMED, instance.markAsPaid(sellPojoMock).getStatus());
-		}		
+		}
+		
+		@Test
+		void markAsPaid_ShouldReturn_SellPojo_WithCorrectDetails() throws BadInputException {
+			// Setup mock objects
+			SellPojo sellPojoMock = SellPojo.builder().build();			
+			
+			SellStatus sellStatusMock = new SellStatus();
+			sellStatusMock.setName(SELL_STATUS_PAYMENT_STARTED);
+			
+			Sell sellMock = new Sell();
+			sellMock.setStatus(sellStatusMock);
+
+			SellDetail sellDetailMock = new SellDetail();
+			sellDetailMock.setId(1L);
+			sellDetailMock.setUnits(11);
+			sellDetailMock.setUnitValue(111);
+			List<SellDetail> sellDetailsMock = List.of(sellDetailMock);
+			
+			ProductPojo productPojoMock = ProductsTestHelper.productPojoAfterCreation();
+			
+			// Stubbing
+			when(crudService.getExisting(any(SellPojo.class))).thenReturn(Optional.of(sellMock)); // fetchExistingOrThrowException		
+			when(sellStatusesRepository.findByName(anyString())).thenReturn(Optional.of(sellStatusMock));			
+			when(converterService.convertToPojo(any())).thenReturn(sellPojoMock); // convertOrThrowException
+			when(sellDetailsRepository.findBySellId(any())).thenReturn(sellDetailsMock);
+			when(productConverterService.convertToPojo(any())).thenReturn(productPojoMock);
+			
+			Collection<SellDetailPojo> actualSellDetailsPojo = instance.markAsPaid(sellPojoMock).getDetails();
+			SellDetailPojo actualSellDetailPojo = actualSellDetailsPojo.iterator().next();
+						
+			assertEquals(1, actualSellDetailsPojo.size());
+			assertEquals(1L, actualSellDetailPojo.getId());
+			assertEquals(11, actualSellDetailPojo.getUnits());
+			assertEquals(111, actualSellDetailPojo.getUnitValue());			
+			assertEquals(productPojoMock, actualSellDetailPojo.getProduct());
+		}
 	}
 	
 	@Nested
@@ -333,7 +384,43 @@ class SalesProcessServiceImplTest {
 			when(converterService.convertToPojo(any())).thenReturn(sellPojoMock); // convertOrThrowException
 						
 			assertEquals(SELL_STATUS_PAID_CONFIRMED, instance.markAsConfirmed(sellPojoMock).getStatus());
-		}		
+		}
+		
+		@Test
+		void markAsConfirmed_ShouldReturn_SellPojo_WithCorrectDetails() throws BadInputException {
+			// Setup mock objects
+			SellPojo sellPojoMock = SellPojo.builder().build();		
+			
+			SellStatus sellStatusMock = new SellStatus();
+			sellStatusMock.setName(SELL_STATUS_PAID_UNCONFIRMED);
+			
+			Sell sellMock = new Sell();
+			sellMock.setStatus(sellStatusMock);
+			
+			SellDetail sellDetailMock = new SellDetail();
+			sellDetailMock.setId(1L);
+			sellDetailMock.setUnits(11);
+			sellDetailMock.setUnitValue(111);
+			List<SellDetail> sellDetailsMock = List.of(sellDetailMock);
+			
+			ProductPojo productPojoMock = ProductsTestHelper.productPojoAfterCreation();
+			
+			// Stubbing
+			when(crudService.getExisting(any(SellPojo.class))).thenReturn(Optional.of(sellMock)); // fetchExistingOrThrowException		
+			when(sellStatusesRepository.findByName(anyString())).thenReturn(Optional.of(sellStatusMock));				
+			when(converterService.convertToPojo(any())).thenReturn(sellPojoMock); // convertOrThrowException
+			when(sellDetailsRepository.findBySellId(any())).thenReturn(sellDetailsMock);
+			when(productConverterService.convertToPojo(any())).thenReturn(productPojoMock);
+			
+			Collection<SellDetailPojo> actualSellDetailsPojo = instance.markAsConfirmed(sellPojoMock).getDetails();
+			SellDetailPojo actualSellDetailPojo = actualSellDetailsPojo.iterator().next();
+						
+			assertEquals(1, actualSellDetailsPojo.size());
+			assertEquals(1L, actualSellDetailPojo.getId());
+			assertEquals(11, actualSellDetailPojo.getUnits());
+			assertEquals(111, actualSellDetailPojo.getUnitValue());
+			assertEquals(productPojoMock, actualSellDetailPojo.getProduct());		
+		}
 	}
 	
 	@Nested
@@ -392,6 +479,42 @@ class SalesProcessServiceImplTest {
 						
 			assertEquals(SELL_STATUS_REJECTED, instance.markAsRejected(sellPojoMock).getStatus());
 		}		
+		
+		@Test
+		void markAsRejected_ShouldReturn_SellPojo_WithCorrectDetails() throws BadInputException {
+			// Setup mock objects
+			SellPojo sellPojoMock = SellPojo.builder().build();			
+			
+			SellStatus sellStatusMock = new SellStatus();
+			sellStatusMock.setName(SELL_STATUS_PAID_UNCONFIRMED);
+			
+			Sell sellMock = new Sell();
+			sellMock.setStatus(sellStatusMock);
+			
+			SellDetail sellDetailMock = new SellDetail();
+			sellDetailMock.setId(1L);
+			sellDetailMock.setUnits(11);
+			sellDetailMock.setUnitValue(111);
+			List<SellDetail> sellDetailsMock = List.of(sellDetailMock);
+			
+			ProductPojo productPojoMock = ProductsTestHelper.productPojoAfterCreation();
+			
+			// Stubbing
+			when(crudService.getExisting(any(SellPojo.class))).thenReturn(Optional.of(sellMock)); // fetchExistingOrThrowException		
+			when(sellStatusesRepository.findByName(anyString())).thenReturn(Optional.of(sellStatusMock));			
+			when(converterService.convertToPojo(any())).thenReturn(sellPojoMock); // convertOrThrowException
+			when(sellDetailsRepository.findBySellId(any())).thenReturn(sellDetailsMock);
+			when(productConverterService.convertToPojo(any())).thenReturn(productPojoMock);
+			
+			Collection<SellDetailPojo> actualSellDetailsPojo = instance.markAsRejected(sellPojoMock).getDetails();
+			SellDetailPojo actualSellDetailPojo = actualSellDetailsPojo.iterator().next();
+						
+			assertEquals(1, actualSellDetailsPojo.size());
+			assertEquals(1L, actualSellDetailPojo.getId());
+			assertEquals(11, actualSellDetailPojo.getUnits());
+			assertEquals(111, actualSellDetailPojo.getUnitValue());
+			assertEquals(productPojoMock, actualSellDetailPojo.getProduct());	
+		}
 	}
 	
 	@Nested
@@ -449,6 +572,42 @@ class SalesProcessServiceImplTest {
 			when(converterService.convertToPojo(any())).thenReturn(sellPojoMock); // convertOrThrowException
 					
 			assertEquals(SELL_STATUS_COMPLETED, instance.markAsCompleted(sellPojoMock).getStatus());
+		}
+		
+		@Test
+		void markAsCompleted_ShouldReturn_SellPojo_WithCorrectDetails() throws BadInputException {
+			// Setup mock objects
+			SellPojo sellPojoMock = SellPojo.builder().build();	
+			
+			SellStatus sellStatusMock = new SellStatus();
+			sellStatusMock.setName(SELL_STATUS_PAID_CONFIRMED);
+			
+			Sell sellMock = new Sell();
+			sellMock.setStatus(sellStatusMock);
+			
+			SellDetail sellDetailMock = new SellDetail();
+			sellDetailMock.setId(1L);
+			sellDetailMock.setUnits(11);
+			sellDetailMock.setUnitValue(111);
+			List<SellDetail> sellDetailsMock = List.of(sellDetailMock);
+			
+			ProductPojo productPojoMock = ProductsTestHelper.productPojoAfterCreation();
+			
+			// Stubbing
+			when(crudService.getExisting(any(SellPojo.class))).thenReturn(Optional.of(sellMock)); // fetchExistingOrThrowException		
+			when(sellStatusesRepository.findByName(anyString())).thenReturn(Optional.of(sellStatusMock));			
+			when(converterService.convertToPojo(any())).thenReturn(sellPojoMock); // convertOrThrowException
+			when(sellDetailsRepository.findBySellId(any())).thenReturn(sellDetailsMock);
+			when(productConverterService.convertToPojo(any())).thenReturn(productPojoMock);
+			
+			Collection<SellDetailPojo> actualSellDetailsPojo = instance.markAsCompleted(sellPojoMock).getDetails();
+			SellDetailPojo actualSellDetailPojo = actualSellDetailsPojo.iterator().next();
+						
+			assertEquals(1, actualSellDetailsPojo.size());
+			assertEquals(1L, actualSellDetailPojo.getId());
+			assertEquals(11, actualSellDetailPojo.getUnits());
+			assertEquals(111, actualSellDetailPojo.getUnitValue());						
+			assertEquals(productPojoMock, actualSellDetailPojo.getProduct());
 		}
 	}
 }
