dda8d731b6b3cb8c60210aff1fedce5c7981ac5e
mepox <21198248+mepox@users.noreply.github.com>
2022-10-27 16:22:16 +0100

    test: add unit test for ProductListJpaCrudServiceImpl

---
+++ src/test/java/org/trebol/jpa/services/crud/ProductListJpaCrudServiceImplTest.java
@@ -0,0 +1,105 @@
+package org.trebol.jpa.services.crud;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.trebol.jpa.entities.ProductList;
+import org.trebol.jpa.repositories.IProductListItemsJpaRepository;
+import org.trebol.jpa.repositories.IProductListsJpaRepository;
+import org.trebol.pojo.ProductListPojo;
+
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Predicate;
+
+@ExtendWith(MockitoExtension.class)
+class ProductListJpaCrudServiceImplTest {
+	
+	@InjectMocks	ProductListJpaCrudServiceImpl instance;
+	@Mock	IProductListsJpaRepository productListRepository;
+	@Mock	IProductListItemsJpaRepository productListItemRepository;
+	
+	@Test
+	void delete_whenProductListNotFound_throwsEntityNotFoundException() {
+		Predicate predicateMock = new BooleanBuilder();
+		
+		when(productListRepository.count(predicateMock)).thenReturn(0L);
+		
+		assertThrows(EntityNotFoundException.class, () -> instance.delete(predicateMock));		
+	}
+	
+	@Test
+	void delete_whenProductListFound_shouldCallDeleteOnRepositories() {
+		Predicate predicateMock = new BooleanBuilder();
+		
+		ProductList productListMock = new ProductList();
+		productListMock.setId(1L);
+		List<ProductList> productListsMock = Arrays.asList(productListMock);		
+		
+		when(productListRepository.count(predicateMock)).thenReturn(1L);
+		when(productListRepository.findAll(predicateMock)).thenReturn(productListsMock);
+		
+		instance.delete(predicateMock);
+		
+		verify(productListItemRepository, times(productListsMock.size())).deleteByListId(1L);
+		verify(productListRepository).deleteAll(productListsMock);
+	}
+	
+	@Test
+	void getExisting_whenIdNull_shouldReturnEmptyOptional() {
+		ProductListPojo productListPojoMock = ProductListPojo.builder()
+				.id(null)
+				.name("productListPojoName")
+				.build();
+		
+		Optional<ProductList> actualProductListOptional = instance.getExisting(productListPojoMock);
+		
+		assertEquals(Optional.empty(), actualProductListOptional);
+	}
+	
+	@Test
+	void getExisting_whenNameNull_shouldReturnEmptyOptional() {
+		ProductListPojo productListPojoMock = ProductListPojo.builder()
+				.id(1L)
+				.name(null)
+				.build();
+		
+		Optional<ProductList> actualProductListOptional = instance.getExisting(productListPojoMock);
+		
+		assertEquals(Optional.empty(), actualProductListOptional);
+	}
+	
+	@Test
+	void getExisting_whenIdAndNameNotNull_shouldReturnProductList() {
+		ProductListPojo productListPojoMock = ProductListPojo.builder()
+				.id(1L)
+				.name("productListPojoName")
+				.build();
+		
+		ProductList productListMock = new ProductList();
+		productListMock.setName("productListName");
+		
+		when(productListRepository.findById(anyLong())).thenReturn(Optional.of(productListMock));
+		
+		Optional<ProductList> actualProductListOptional = instance.getExisting(productListPojoMock);
+		
+		assertTrue(actualProductListOptional.isPresent());
+		assertEquals(productListMock, actualProductListOptional.get());
+	}	
+
+}
