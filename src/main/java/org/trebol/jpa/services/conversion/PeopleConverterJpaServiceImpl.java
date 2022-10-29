d8a2bf8da91cd2821817bd4cf99e593f9e5fe08a
JellyBoonz <86168103+JellyBoonz@users.noreply.github.com>
2022-10-29 14:55:24 -0400

    Update PeopleConverterJpaServiceImpl.java

--- src/main/java/org/trebol/jpa/services/conversion/PeopleConverterJpaServiceImpl.java
+++ src/main/java/org/trebol/jpa/services/conversion/PeopleConverterJpaServiceImpl.java
@@ -72,12 +72,12 @@ public class PeopleConverterJpaServiceImpl
 
     // phones may be empty, but not null
     String phone1 = source.getPhone1();
-    if (phone1 != null && !target.getPhone1().equals(phone1)) {
+    if (!phone1.equals(null) && !target.getPhone1().equals(phone1)) {
         target.setPhone1(phone1);
     }
 
     String phone2 = source.getPhone2();
-    if (phone2 != null && !target.getPhone2().equals(phone2)) {
+    if (!phone2.equals(null) && !target.getPhone2().equals(phone2)) {
         target.setPhone2(phone2);
     }
 
