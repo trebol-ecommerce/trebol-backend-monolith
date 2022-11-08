932a0cc19f65af2314259feb9379e48af9d8ea3a
bglamadrid <bg.lamadrid@gmail.com>
2022-11-07 23:27:06 -0300

    feat: implement `IDataTransportJpaService` (broken)
    ProductCategoriesDataTransportJpaServiceImpl will not compile
    left a TODO on it

---
+++ src/main/java/org/trebol/jpa/services/datatransport/BillingTypesDataTransportJpaServiceImpl.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2022 The Trebol eCommerce Project
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished
+ * to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package org.trebol.jpa.services.datatransport;
+
+import org.springframework.stereotype.Service;
+import org.trebol.exceptions.BadInputException;
+import org.trebol.jpa.entities.BillingType;
+import org.trebol.jpa.services.IDataTransportJpaService;
+import org.trebol.pojo.BillingTypePojo;
+
+@Service
+public class BillingTypesDataTransportJpaServiceImpl
+  implements IDataTransportJpaService<BillingTypePojo, BillingType> {
+
+  public BillingTypesDataTransportJpaServiceImpl(
+  ) {
+  }
+
+  @Override
+  public BillingType applyChangesToExistingEntity(BillingTypePojo source, BillingType existing) throws BadInputException {
+    BillingType target = new BillingType(existing);
+
+    String name = source.getName();
+    if (!name.isBlank() && !target.getName().equals(name)) {
+      target.setName(name);
+    }
+
+    return target;
+  }
+}
---
+++ src/main/java/org/trebol/jpa/services/datatransport/CustomersDataTransportJpaServiceImpl.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2022 The Trebol eCommerce Project
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished
+ * to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package org.trebol.jpa.services.datatransport;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.trebol.exceptions.BadInputException;
+import org.trebol.jpa.entities.Customer;
+import org.trebol.jpa.entities.Person;
+import org.trebol.jpa.services.IDataTransportJpaService;
+import org.trebol.pojo.CustomerPojo;
+import org.trebol.pojo.PersonPojo;
+
+@Service
+public class CustomersDataTransportJpaServiceImpl
+  implements IDataTransportJpaService<CustomerPojo, Customer> {
+
+  private final IDataTransportJpaService<PersonPojo, Person> peopleService;
+
+  @Autowired
+  public CustomersDataTransportJpaServiceImpl(IDataTransportJpaService<PersonPojo, Person> peopleService) {
+    this.peopleService = peopleService;
+  }
+
+  @Override
+  public Customer applyChangesToExistingEntity(CustomerPojo source, Customer existing) throws BadInputException {
+    Customer target = new Customer(existing);
+    Person existingPerson = existing.getPerson();
+
+    PersonPojo sourcePerson = source.getPerson();
+    Person person = peopleService.applyChangesToExistingEntity(sourcePerson, existingPerson);
+    target.setPerson(person);
+
+    return target;
+  }
+}
