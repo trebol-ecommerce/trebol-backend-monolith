e04baf1b7da868afe8a8821819e3ab4e74d0ba07
bglamadrid <bg.lamadrid@gmail.com>
2022-11-08 22:41:14 -0300

    test: introduce unit tests for data transport services

---
+++ src/test/java/org/trebol/jpa/services/datatransport/ImagesDataTransportJpaServiceImplTest.java
@@ -0,0 +1,62 @@
+package org.trebol.jpa.services.datatransport;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.trebol.exceptions.BadInputException;
+import org.trebol.jpa.entities.Image;
+import org.trebol.pojo.ImagePojo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.trebol.constant.TestConstants.ANY;
+
+@ExtendWith(MockitoExtension.class)
+class ImagesDataTransportJpaServiceImplTest {
+
+    @InjectMocks
+    private ImagesDataTransportJpaServiceImpl sut;
+
+    private Image image;
+    private ImagePojo imagePojo;
+
+    @BeforeEach
+    void beforeEach() {
+        image = new Image();
+        image.setId(1L);
+        image.setFilename(ANY);
+
+        imagePojo = ImagePojo.builder()
+          .id(1L)
+          .filename(ANY)
+          .build();
+    }
+
+    @AfterEach
+    void afterEach() {
+        image = null;
+        imagePojo = null;
+    }
+
+    @Test
+    void testApplyChangesToExistingEntity() throws BadInputException {
+        Image actual = sut.applyChangesToExistingEntity(imagePojo, image);
+        assertEquals(1L, actual.getId());
+
+        image.setFilename(ANY);
+        image.setCode(ANY);
+        image.setUrl(ANY);
+
+        imagePojo.setUrl(ANY + " ");
+        imagePojo.setCode(ANY + " ");
+        imagePojo.setFilename(ANY + " ");
+
+        actual = sut.applyChangesToExistingEntity(imagePojo, image);
+
+        assertEquals(ANY + " ", actual.getUrl());
+        assertEquals(ANY + " ", actual.getCode());
+        assertEquals(ANY + " ", actual.getFilename());
+    }
+}
