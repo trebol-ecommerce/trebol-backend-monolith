33207a9fc63ecfe74623090dea42ad02b7fb1a83
bglamadrid <bg.lamadrid@gmail.com>
2022-11-18 13:27:05 -0300

    chore: add `applyChangesToExistingEntity` back with a deprecation note

--- src/main/java/org/trebol/jpa/services/ITwoWayConverterJpaService.java
+++ src/main/java/org/trebol/jpa/services/ITwoWayConverterJpaService.java
@@ -41,6 +41,19 @@ public interface ITwoWayConverterJpaService<P, E> {
   E convertToNewEntity(P source) throws BadInputException;
 
   /**
+   * Creates a clone @Entity, then updates it with new data from a Pojo class,
+   * putting differences in properties one-by-one. It does not include relationships to other entities.
+   * This method DOES NOT persist data.
+   * @param source The Pojo containing data updates.
+   * @param target The target entity.
+   * @return An updated instance of the @Entity, prepared to be saved to the database.
+   * @throws BadInputException If the object with changes has invalid values
+   * @deprecated Please use {@link IDataTransportJpaService}'s method instead
+   */
+  @Deprecated
+  E applyChangesToExistingEntity(P source, E target) throws BadInputException;
+
+   /**
    * Converts an existing @Entity to its complete Pojo equivalent, including all relationships to other entities.
    * @param source The source @Entity.
    * @return The resulting Pojo, or null if the @Entity isn't persistent
