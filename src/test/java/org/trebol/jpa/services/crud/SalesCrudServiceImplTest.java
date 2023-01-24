1f51e4e9ed53248e4641a35c4bb728e3b18b11d8
bglamadrid <bg.lamadrid@gmail.com>
2023-01-23 23:56:08 -0300

    test: fix `SalesCrudServiceImplTest`

--- src/test/java/org/trebol/jpa/services/crud/SalesCrudServiceImplTest.java
+++ src/test/java/org/trebol/jpa/services/crud/SalesCrudServiceImplTest.java
@@ -10,13 +10,18 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.trebol.exceptions.BadInputException;
-import org.trebol.jpa.entities.Sell;
+import org.trebol.jpa.entities.*;
+import org.trebol.jpa.repositories.AddressesJpaRepository;
+import org.trebol.jpa.repositories.BillingTypesJpaRepository;
 import org.trebol.jpa.repositories.ProductsJpaRepository;
 import org.trebol.jpa.repositories.SalesJpaRepository;
-import org.trebol.jpa.services.conversion.ProductsConverterService;
-import org.trebol.jpa.services.conversion.SalesConverterService;
+import org.trebol.jpa.services.conversion.*;
 import org.trebol.jpa.services.datatransport.SalesDataTransportService;
+import org.trebol.pojo.AddressPojo;
+import org.trebol.pojo.CustomerPojo;
+import org.trebol.pojo.SellDetailPojo;
 import org.trebol.pojo.SellPojo;
+import org.trebol.testhelpers.CustomersTestHelper;
 import org.trebol.testhelpers.ProductsTestHelper;
 import org.trebol.testhelpers.SalesTestHelper;
 
@@ -31,6 +36,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.trebol.constant.TestConstants.ANY;
 
 @ExtendWith(MockitoExtension.class)
 class SalesCrudServiceImplTest {
@@ -38,10 +44,19 @@ class SalesCrudServiceImplTest {
   @Mock SalesJpaRepository salesRepositoryMock;
   @Mock ProductsJpaRepository productsRepositoryMock;
   @Mock SalesConverterService salesConverterMock;
-  @Mock SalesDataTransportService dataTransportServiceMock;
-  @Mock ProductsConverterService productsConverterMock; // TODO write an unit test that needs this mock
+  @Mock SalesDataTransportService salesDataTransportServiceMock;
+  @Mock ProductsConverterService productsConverterServiceMock; // TODO write an unit test that needs this mock
+  @Mock CustomersCrudService customersCrudServiceMock;
+  @Mock CustomersConverterService customersConverterServiceMock;
+  @Mock BillingTypesJpaRepository billingTypesRepositoryMock;
+  @Mock BillingCompaniesCrudService billingCompaniesCrudServiceMock;
+  @Mock BillingCompaniesConverterService billingCompaniesConverterServiceMock;
+  @Mock AddressesJpaRepository addressesRepositoryMock;
+  @Mock ShippersCrudService shippersCrudServiceMock;
+  @Mock AddressesConverterService addressesConverterServiceMock;
   ProductsTestHelper productsHelper = new ProductsTestHelper();
   SalesTestHelper salesHelper = new SalesTestHelper();
+  CustomersTestHelper customersHelper = new CustomersTestHelper();
 
   @BeforeEach
   void beforeEach() {
@@ -78,14 +93,59 @@ class SalesCrudServiceImplTest {
   }
 
   @Test
+  void does_not_create_sales_with_incomplete_data() {
+    List.of(
+      SellPojo.builder().build(),
+      SellPojo.builder()
+        .customer(customersHelper.customerPojoBeforeCreation())
+        .build(),
+      SellPojo.builder()
+        .customer(customersHelper.customerPojoBeforeCreation())
+        .billingType(ANY)
+        .build(),
+      SellPojo.builder()
+        .customer(customersHelper.customerPojoBeforeCreation())
+        .billingType(ANY)
+        .billingAddress(AddressPojo.builder().build())
+        .build(),
+      SellPojo.builder()
+        .customer(customersHelper.customerPojoBeforeCreation())
+        .billingType(ANY)
+        .billingAddress(AddressPojo.builder().build())
+        .details(List.of())
+        .build(),
+      salesHelper.sellPojoBeforeCreation()
+    ).forEach((sellPojo) -> {
+      assertThrows(BadInputException.class, () -> instance.create(sellPojo));
+    });
+  }
+
+  @Test
   void creates_sell()
     throws BadInputException, EntityExistsException {
-    SellPojo input = salesHelper.sellPojoBeforeCreation();
+    SellPojo input = SellPojo.builder()
+      .customer(customersHelper.customerPojoBeforeCreation())
+      .billingType(ANY)
+      .billingAddress(AddressPojo.builder().build())
+      .details(List.of(
+        SellDetailPojo.builder()
+          .units(1)
+          .product(productsHelper.productPojoBeforeCreation())
+          .build()
+      ))
+      .build();
     SellPojo expectedResult = salesHelper.sellPojoAfterCreation();
-
-    when(salesConverterMock.convertToNewEntity(any(SellPojo.class))).thenReturn(salesHelper.sellEntityBeforeCreation());
-    when(productsRepositoryMock.findByBarcode(anyString())).thenReturn(Optional.of(productsHelper.productEntityAfterCreation()));
-    when(salesRepositoryMock.saveAndFlush(any(Sell.class))).thenReturn(salesHelper.sellEntityAfterCreation());
+    Customer customerEntityInput = customersHelper.customerEntityBeforeCreation();
+    Sell sellEntityInput = salesHelper.sellEntityBeforeCreation();
+    Product productEntityInput = productsHelper.productEntityAfterCreation();
+    Sell sellEntityOutput = salesHelper.sellEntityAfterCreation();
+    when(salesConverterMock.convertToNewEntity(any(SellPojo.class))).thenReturn(sellEntityInput);
+    when(customersCrudServiceMock.getExisting(any(CustomerPojo.class))).thenReturn(Optional.empty());
+    when(customersConverterServiceMock.convertToNewEntity(any(CustomerPojo.class))).thenReturn(customerEntityInput);
+    when(billingTypesRepositoryMock.findByName(anyString())).thenReturn(Optional.of(new BillingType()));
+    when(addressesConverterServiceMock.convertToNewEntity(any(AddressPojo.class))).thenReturn(new Address());
+    when(productsRepositoryMock.findByBarcode(anyString())).thenReturn(Optional.of(productEntityInput));
+    when(salesRepositoryMock.saveAndFlush(any(Sell.class))).thenReturn(sellEntityOutput);
     when(salesConverterMock.convertToPojo(any(Sell.class))).thenReturn(expectedResult);
 
     SellPojo result = instance.create(input);
@@ -103,7 +163,7 @@ class SalesCrudServiceImplTest {
     Sell internalResult = new Sell(salesHelper.sellEntityAfterCreation());
     internalResult.setDate(updatedDate);
     when(salesRepositoryMock.findOne(any(Predicate.class))).thenReturn(Optional.of(salesHelper.sellEntityAfterCreation()));
-    when(dataTransportServiceMock.applyChangesToExistingEntity(any(SellPojo.class), any(Sell.class))).thenReturn(internalResult);
+    when(salesDataTransportServiceMock.applyChangesToExistingEntity(any(SellPojo.class), any(Sell.class))).thenReturn(internalResult);
     when(salesRepositoryMock.saveAndFlush(any(Sell.class))).thenReturn(internalResult);
     when(salesConverterMock.convertToPojo(any(Sell.class))).thenReturn(input);
 
@@ -119,11 +179,11 @@ class SalesCrudServiceImplTest {
     SellPojo input = salesHelper.sellPojoAfterCreation();
     Sell matchingEntity = salesHelper.sellEntityAfterCreation();
     when(salesRepositoryMock.findOne(any(Predicate.class))).thenReturn(Optional.of(matchingEntity));
-    when(dataTransportServiceMock.applyChangesToExistingEntity(any(SellPojo.class), any(Sell.class))).thenReturn(matchingEntity);
+    when(salesDataTransportServiceMock.applyChangesToExistingEntity(any(SellPojo.class), any(Sell.class))).thenReturn(matchingEntity);
 
     SellPojo result = instance.update(input, new BooleanBuilder());
 
-    verify(dataTransportServiceMock).applyChangesToExistingEntity(input, matchingEntity);
+    verify(salesDataTransportServiceMock).applyChangesToExistingEntity(input, matchingEntity);
     assertEquals(input, result);
   }
 
