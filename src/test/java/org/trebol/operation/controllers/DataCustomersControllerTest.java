39e61bb4e4920d12b2f9e649a61e2814aabc5d17
bglamadrid <bg.lamadrid@gmail.com>
2023-01-24 21:29:48 -0300

    test: add tests for `DataCustomersController`

--- src/test/java/org/trebol/operation/controllers/DataCustomersControllerTest.java
+++ src/test/java/org/trebol/operation/controllers/DataCustomersControllerTest.java
@@ -1,23 +1,40 @@
 package org.trebol.operation.controllers;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.trebol.jpa.entities.Customer;
-import org.trebol.jpa.services.CrudGenericService;
 import org.trebol.jpa.services.PredicateService;
 import org.trebol.jpa.services.SortSpecService;
+import org.trebol.jpa.services.crud.CustomersCrudService;
 import org.trebol.operation.services.PaginationService;
 import org.trebol.pojo.CustomerPojo;
+import org.trebol.pojo.DataPagePojo;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class DataCustomersControllerTest {
   @InjectMocks DataCustomersController instance;
-  @Mock PaginationService paginationService;
-  @Mock SortSpecService<Customer> sortService;
-  @Mock CrudGenericService<CustomerPojo, Customer> crudService;
-  @Mock PredicateService<Customer> predicateService;
+  @Mock PaginationService paginationServiceMock;
+  @Mock SortSpecService<Customer> sortServiceMock;
+  @Mock CustomersCrudService crudServiceMock;
+  @Mock PredicateService<Customer> predicateServiceMock;
 
-  // TODO write a test
+  @Test
+  void reads_customers() {
+    DataPagePojo<CustomerPojo> pagePojo = new DataPagePojo<>(0, 0);
+    when(crudServiceMock.readMany(anyInt(), anyInt(), eq(null), eq(null))).thenReturn(pagePojo);
+    DataPagePojo<CustomerPojo> result = instance.readMany(Map.of());
+    assertNotNull(result);
+    assertEquals(0, result.getTotalCount());
+    assertTrue(result.getItems().isEmpty());
+  }
 }
