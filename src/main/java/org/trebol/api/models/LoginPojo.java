db034084574418db47eb7410938d17014c67785c
bglamadrid <bg.lamadrid@gmail.com>
2023-02-16 16:25:34 -0300

    test: cover `JwtMockLoginAuthenticationFilter`

--- src/main/java/org/trebol/api/models/LoginPojo.java
+++ src/main/java/org/trebol/api/models/LoginPojo.java
@@ -21,14 +21,16 @@
 package org.trebol.api.models;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 
 @Data
-@AllArgsConstructor
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class LoginPojo {
   @NotBlank
   private String name;
