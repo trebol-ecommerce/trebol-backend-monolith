d3d9cef53a380512407885968697380bb80f934e
bglamadrid <bg.lamadrid@gmail.com>
2023-04-25 20:10:00 -0400

    feat: implement new `patchExistingEntity` signature in User service

--- src/test/java/org/trebol/jpa/services/patch/impl/UsersPatchServiceImplTest.java
+++ src/test/java/org/trebol/jpa/services/patch/impl/UsersPatchServiceImplTest.java
@@ -20,82 +20,105 @@
 
 package org.trebol.jpa.services.patch.impl;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.trebol.api.models.PersonPojo;
 import org.trebol.api.models.UserPojo;
 import org.trebol.common.exceptions.BadInputException;
-import org.trebol.jpa.entities.Person;
 import org.trebol.jpa.entities.User;
 import org.trebol.jpa.entities.UserRole;
-import org.trebol.jpa.repositories.PeopleRepository;
 import org.trebol.jpa.repositories.UserRolesRepository;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 import static org.trebol.testing.TestConstants.ANY;
+import static org.trebol.testing.TestConstants.NOT_ANY;
 
 @ExtendWith(MockitoExtension.class)
 class UsersPatchServiceImplTest {
   @InjectMocks UsersPatchServiceImpl instance;
   @Mock UserRolesRepository rolesRepositoryMock;
-  @Mock PeopleRepository peopleRepositoryMock;
-  @Mock PasswordEncoder passwordEncoderMock;
-  User user;
-  UserPojo userPojo;
+  private static ObjectMapper MAPPER;
+  private static User EXISTING_USER;
 
-  @BeforeEach
-  void beforeEach() {
-    user = new User();
-    user.setName(ANY);
-    user.setId(1L);
-    final UserRole userRole = new UserRole();
-    userRole.setName(ANY);
-    user.setUserRole(userRole);
-    Person person = new Person();
-    user.setPerson(person);
-    userPojo = UserPojo.builder()
+  @BeforeAll
+  static void beforeAll() {
+    MAPPER = new ObjectMapper()
+      .setSerializationInclusion(NON_NULL);
+    EXISTING_USER = User.builder()
       .id(1L)
       .name(ANY)
+      .userRole(UserRole.builder()
+        .id(1L)
+        .name(ANY)
+        .build())
+      .password(null)
+      .person(null)
       .build();
   }
 
-  @AfterEach
-  void afterEach() {
-    user = null;
-    userPojo = null;
+  @Test
+  void performs_empty_patch() throws BadInputException {
+    Map<String, Object> input = this.mapFrom(UserPojo.builder().build());
+    User result = instance.patchExistingEntity(input, EXISTING_USER);
+    assertEquals(EXISTING_USER, result);
   }
 
   @Test
-  void patches_entity_data() throws BadInputException {
-    userPojo.setId(1L);
-    userPojo.setName(ANY);
-    userPojo.setRole(ANY);
-    userPojo.setPassword(ANY);
-    final PersonPojo personPojo = PersonPojo.builder().idNumber(ANY).build();
-    userPojo.setPerson(personPojo);
-    user.setId(1L);
-    user.setName(ANY + " ");
-    final UserRole role = new UserRole();
-    role.setName(ANY + " ");
-    user.setUserRole(role);
-    user.setPassword(ANY + " ");
-    final Person person = new Person();
-    person.setIdNumber(ANY + " ");
-    user.setPerson(person);
-    when(rolesRepositoryMock.findByName(anyString())).thenReturn(Optional.of(role));
-    when(passwordEncoderMock.encode(anyString())).thenReturn(ANY);
-    when(peopleRepositoryMock.findByIdNumber(anyString())).thenReturn(Optional.of(person));
-    User actual = instance.patchExistingEntity(userPojo, user);
-    assertEquals(ANY + " ", actual.getPerson().getIdNumber());
+  void patches_name() throws BadInputException {
+    Map<String, Object> input = this.mapFrom(UserPojo.builder()
+      .name(NOT_ANY)
+      .build());
+    User result = instance.patchExistingEntity(input, EXISTING_USER);
+    assertNotEquals(EXISTING_USER, result);
+    assertEquals(NOT_ANY, result.getName());
+  }
+
+  @Test
+  void patches_role() throws BadInputException {
+    UserRole existingRole = UserRole.builder().build();
+    Map<String, Object> input = this.mapFrom(UserPojo.builder()
+      .role(NOT_ANY)
+      .build());
+    when(rolesRepositoryMock.findByName(anyString())).thenReturn(Optional.of(existingRole));
+    User result = instance.patchExistingEntity(input, EXISTING_USER);
+    assertNotEquals(EXISTING_USER, result);
+    assertEquals(existingRole, result.getUserRole());
+  }
+
+  @Test
+  void patches_all_fields() throws BadInputException {
+    UserRole existingRole = UserRole.builder().build();
+    Map<String, Object> input = this.mapFrom(UserPojo.builder()
+      .name(NOT_ANY)
+      .role(NOT_ANY)
+      .build());
+    when(rolesRepositoryMock.findByName(anyString())).thenReturn(Optional.of(existingRole));
+    User result = instance.patchExistingEntity(input, EXISTING_USER);
+    assertNotEquals(EXISTING_USER, result);
+    assertEquals(NOT_ANY, result.getName());
+    assertEquals(existingRole, result.getUserRole());
+  }
+
+  @Test
+  void does_not_support_old_method_signature() {
+    UserPojo input = UserPojo.builder().build();
+    assertThrows(UnsupportedOperationException.class,
+      () -> instance.patchExistingEntity(input, EXISTING_USER));
+  }
+
+  private Map<String, Object> mapFrom(UserPojo data) {
+    return MAPPER.convertValue(data, new TypeReference<HashMap<String, Object>>() {});
   }
 }
