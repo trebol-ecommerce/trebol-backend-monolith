97276fa43ea271173a90672577debeda2c6af2c7
bglamadrid <bg.lamadrid@gmail.com>
2023-04-25 20:24:17 -0400

    test: update `CrudGeneric` service suites
    refactor `GenericEntity` and `GenericPojo`
    as inner static classes of the test class

--- src/test/java/org/trebol/jpa/services/CrudGenericServiceTest.java
+++ src/test/java/org/trebol/jpa/services/CrudGenericServiceTest.java
@@ -22,6 +22,8 @@ package org.trebol.jpa.services;
 
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.Predicate;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -36,21 +38,24 @@ import org.trebol.jpa.services.crud.CrudGenericService;
 import javax.persistence.EntityExistsException;
 import javax.persistence.EntityNotFoundException;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyMap;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class CrudGenericServiceTest {
+  static final String GENERIC_NAME = "test";
   @Mock Repository<GenericEntity> genericRepositoryMock;
   @Mock ConverterService<GenericPojo, GenericEntity> genericConverterMock;
   @Mock PatchService<GenericPojo, GenericEntity> genericPatchServiceMock;
-  final GenericPojo newPojo = new GenericPojo(null, "test");
-  final GenericEntity newEntity = new GenericEntity(null, "test");
-  final GenericEntity persistedEntity = new GenericEntity(1L, "test");
-  final GenericPojo persistedPojo = new GenericPojo(1L, "test");
+  final GenericPojo newPojo = new GenericPojo(null, GENERIC_NAME);
+  final GenericEntity newEntity = new GenericEntity(null, GENERIC_NAME);
+  final GenericEntity persistedEntity = new GenericEntity(1L, GENERIC_NAME);
+  final GenericPojo persistedPojo = new GenericPojo(1L, GENERIC_NAME);
   final List<GenericEntity> emptyEntityList = List.of();
   final List<GenericEntity> persistedEntityList = List.of(persistedEntity);
   final List<GenericPojo> persistedPojoList = List.of(persistedPojo);
@@ -134,41 +139,48 @@ class CrudGenericServiceTest {
   }
 
   @Test
-  void updates_data()
+  void partially_updates_data()
     throws BadInputException, EntityNotFoundException {
-    GenericPojo updatingPojo = new GenericPojo(1L, "test2");
-    GenericEntity updatedEntity = new GenericEntity(1L, "test2");
-    when(genericPatchServiceMock.patchExistingEntity(updatingPojo, persistedEntity)).thenReturn(updatedEntity);
+    Long id = 1L;
+    String name = "test2";
+    Map<String, Object> changes = Map.of("name", name);
+    GenericEntity updatedEntity = new GenericEntity(id, name);
+    GenericPojo updatedPojo = new GenericPojo(id, name);
+    when(genericPatchServiceMock.patchExistingEntity(anyMap(), persistedEntity)).thenReturn(updatedEntity);
     when(genericRepositoryMock.saveAndFlush(updatedEntity)).thenReturn(updatedEntity);
-    when(genericConverterMock.convertToPojo(updatedEntity)).thenReturn(updatingPojo);
+    when(genericConverterMock.convertToPojo(updatedEntity)).thenReturn(updatedPojo);
     CrudGenericService<GenericPojo, GenericEntity> service = this.instantiate_with_existing_entity();
 
-    GenericPojo result = service.update(updatingPojo);
+    Optional<GenericPojo> result = service.partialUpdate(changes, id);
 
-    assertNotNull(result);
-    assertEquals(updatingPojo, result);
-    verify(genericPatchServiceMock).patchExistingEntity(updatingPojo, persistedEntity);
+    assertTrue(result.isPresent());
+    assertEquals(updatedPojo, result.get());
+    verify(genericPatchServiceMock).patchExistingEntity(changes, persistedEntity);
     verify(genericRepositoryMock).saveAndFlush(updatedEntity);
     verify(genericConverterMock).convertToPojo(updatedEntity);
   }
 
   @Test
-  void updates_data_using_filters()
+  void partially_updates_data_using_filters()
     throws BadInputException, EntityNotFoundException {
+    Long id = 1L;
+    String name = "test2";
     Predicate filters = new BooleanBuilder();
-    GenericPojo updatingPojo = new GenericPojo(1L, "test2");
-    GenericEntity updatedEntity = new GenericEntity(1L, "test2");
+    Map<String, Object> changes = Map.of("name", name);
+    GenericEntity updatedEntity = new GenericEntity(1L, name);
+    GenericPojo updatedPojo = new GenericPojo(id, name);
     when(genericRepositoryMock.findOne(filters)).thenReturn(Optional.of(persistedEntity));
-    when(genericPatchServiceMock.patchExistingEntity(updatingPojo, persistedEntity)).thenReturn(updatedEntity);
+    when(genericPatchServiceMock.patchExistingEntity(anyMap(), persistedEntity)).thenReturn(updatedEntity);
     when(genericRepositoryMock.saveAndFlush(updatedEntity)).thenReturn(updatedEntity);
-    when(genericConverterMock.convertToPojo(updatedEntity)).thenReturn(updatingPojo);
+    when(genericConverterMock.convertToPojo(updatedEntity)).thenReturn(updatedPojo);
 
     CrudGenericService<GenericPojo, GenericEntity> service = this.instantiate_with_existing_entity();
-    GenericPojo result = service.update(updatingPojo, filters);
+    Optional<GenericPojo> result = service.partialUpdate(changes, filters);
 
-    assertEquals(updatingPojo, result);
+    assertTrue(result.isPresent());
+    assertEquals(updatedPojo, result.get());
     verify(genericRepositoryMock).findOne(filters);
-    verify(genericPatchServiceMock).patchExistingEntity(updatingPojo, persistedEntity);
+    verify(genericPatchServiceMock).patchExistingEntity(changes, persistedEntity);
     verify(genericRepositoryMock).saveAndFlush(updatedEntity);
     verify(genericConverterMock).convertToPojo(updatedEntity);
   }
@@ -211,6 +223,22 @@ class CrudGenericServiceTest {
       genericRepositoryMock,
       genericConverterMock,
       genericPatchServiceMock) {
+
+      @Override
+      public GenericPojo update(GenericPojo input) throws EntityNotFoundException, BadInputException {
+        throw new UnsupportedOperationException("This method signature has been deprecated");
+      }
+
+      @Override
+      protected Long extractIdFrom(GenericEntity source) {
+        return source.id;
+      }
+
+      @Override
+      protected void injectIdInto(Long id, GenericEntity target) {
+        target.id = id;
+      }
+
       @Override
       public Optional<GenericEntity> getExisting(GenericPojo example) {
         return Optional.empty();
@@ -223,10 +251,40 @@ class CrudGenericServiceTest {
       genericRepositoryMock,
       genericConverterMock,
       genericPatchServiceMock) {
+
+      @Override
+      public GenericPojo update(GenericPojo input) throws EntityNotFoundException, BadInputException {
+        throw new UnsupportedOperationException("This method signature has been deprecated");
+      }
+
+      @Override
+      protected Long extractIdFrom(GenericEntity source) {
+        return source.id;
+      }
+
+      @Override
+      protected void injectIdInto(Long id, GenericEntity target) {
+        target.id = id;
+      }
+
       @Override
       public Optional<GenericEntity> getExisting(GenericPojo example) {
         return Optional.of(persistedEntity);
       }
     };
+  }
+
+  @Data
+  @AllArgsConstructor
+  static class GenericPojo {
+    Long id;
+    String name;
+  }
+
+  @Data
+  @AllArgsConstructor
+  static class GenericEntity {
+    Long id;
+    String name;
   }
 }
